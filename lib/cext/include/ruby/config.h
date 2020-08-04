509fd944dd19e01164ec460edbf432ee8b387099
seadu <68719109+seadu@users.noreply.github.com>
2020-08-04 12:37:56 +0000

    Add cli

---
+++ lib/cext/include/ruby/config.h
@@ -0,0 +1,22 @@
+// See updating-ruby.md for instructions on regenerating config headers.
+#ifdef __APPLE__
+
+#if defined(__x86_64__)
+#include <truffleruby/config_darwin_amd64.h>
+#elif defined(__aarch64__)
+#include <truffleruby/config_darwin_aarch64.h>
+#else
+#error Unsupported platform
+#endif
+
+#else
+
+#if defined(__x86_64__)
+#include <truffleruby/config_linux_amd64.h>
+#elif defined(__aarch64__)
+#include <truffleruby/config_linux_aarch64.h>
+#else
+#error Unsupported platform
+#endif
+
+#endif
---
+++ src/main/java/org/truffleruby/core/IsNilNode.java
@@ -0,0 +1,37 @@
+
+/*
+ * Copyright (c) 2014, 2023 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 2.0, or
+ * GNU General Public License version 2, or
+ * GNU Lesser General Public License version 2.1.
+ */
+package org.truffleruby.core;
+
+import org.truffleruby.language.RubyContextSourceNode;
+import org.truffleruby.language.RubyNode;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+public class IsNilNode extends RubyContextSourceNode {
+
+    @Child private RubyNode child;
+
+    public IsNilNode(RubyNode child) {
+        this.child = child;
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        return child.execute(frame) == nil;
+    }
+
+    @Override
+    public RubyNode cloneUninitialized() {
+        var copy = new IsNilNode(child.cloneUninitialized());
+        return copy.copyFlags(this);
+    }
+
+}
