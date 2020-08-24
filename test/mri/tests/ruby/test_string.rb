eeb557514e64f6caa010c7f383afca5d12a7b649
seadu <68719109+seadu@users.noreply.github.com>
2020-08-24 09:45:38 +0000

    merging dev

--- test/mri/tests/ruby/test_string.rb
+++ test/mri/tests/ruby/test_string.rb
@@ -79,4 +79,236 @@ class TestString < Test::Unit::TestCase
     str = S("")
     assert_equal("mystring", str.__send__(:initialize, "mystring", capacity: 1000))
     str = S("mystring")
-    assert_equal("mystring", str.__se
+    assert_equal("mystring", str.__send__(:initialize, str, capacity: 1000))
+  end
+
+  def test_initialize_shared
+    String.new(str = "mystring" * 10).__send__(:initialize, capacity: str.bytesize)
+    assert_equal("mystring", str[0, 8])
+  end
+
+  def test_initialize_nonstring
+    assert_raise(TypeError) {
+      S(1)
+    }
+    assert_raise(TypeError) {
+      S(1, capacity: 1000)
+    }
+  end
+
+  def test_initialize_memory_leak
+    assert_no_memory_leak([], <<-PREP, <<-CODE, rss: true)
+code = proc {('x'*100000).__send__(:initialize, '')}
+1_000.times(&code)
+PREP
+100_000.times(&code)
+CODE
+  end
+
+  # Bug #18154
+  def test_initialize_nofree_memory_leak
+    assert_no_memory_leak([], <<-PREP, <<-CODE, rss: true)
+code = proc {0.to_s.__send__(:initialize, capacity: 10000)}
+1_000.times(&code)
+PREP
+100_000.times(&code)
+CODE
+  end
+
+  def test_AREF # '[]'
+    assert_equal("A",  S("AooBar")[0])
+    assert_equal("B",  S("FooBaB")[-1])
+    assert_equal(nil, S("FooBar")[6])
+    assert_equal(nil, S("FooBar")[-7])
+
+    assert_equal(S("Foo"), S("FooBar")[0,3])
+    assert_equal(S("Bar"), S("FooBar")[-3,3])
+    assert_equal(S(""),    S("FooBar")[6,2])
+    assert_equal(nil,      S("FooBar")[-7,10])
+
+    assert_equal(S("Foo"), S("FooBar")[0..2])
+    assert_equal(S("Foo"), S("FooBar")[0...3])
+    assert_equal(S("Bar"), S("FooBar")[-3..-1])
+    assert_equal(S(""),    S("FooBar")[6..2])
+    assert_equal(nil,      S("FooBar")[-10..-7])
+
+    assert_equal(S("Foo"), S("FooBar")[/^F../])
+    assert_equal(S("Bar"), S("FooBar")[/..r$/])
+    assert_equal(nil,      S("FooBar")[/xyzzy/])
+    assert_equal(nil,      S("FooBar")[/plugh/])
+
+    assert_equal(S("Foo"), S("FooBar")[S("Foo")])
+    assert_equal(S("Bar"), S("FooBar")[S("Bar")])
+    assert_equal(nil,      S("FooBar")[S("xyzzy")])
+    assert_equal(nil,      S("FooBar")[S("plugh")])
+
+    if @aref_re_nth
+      assert_equal(S("Foo"), S("FooBar")[/([A-Z]..)([A-Z]..)/, 1])
+      assert_equal(S("Bar"), S("FooBar")[/([A-Z]..)([A-Z]..)/, 2])
+      assert_equal(nil,      S("FooBar")[/([A-Z]..)([A-Z]..)/, 3])
+      assert_equal(S("Bar"), S("FooBar")[/([A-Z]..)([A-Z]..)/, -1])
+      assert_equal(S("Foo"), S("FooBar")[/([A-Z]..)([A-Z]..)/, -2])
+      assert_equal(nil,      S("FooBar")[/([A-Z]..)([A-Z]..)/, -3])
+    end
+
+    o = Object.new
+    def o.to_int; 2; end
+    assert_equal("o", "foo"[o])
+
+    assert_raise(ArgumentError) { "foo"[] }
+  end
+
+  def test_ASET # '[]='
+    s = S("FooBar")
+    s[0] = S('A')
+    assert_equal(S("AooBar"), s)
+
+    s[-1]= S('B')
+    assert_equal(S("AooBaB"), s)
+    assert_raise(IndexError) { s[-7] = S("xyz") }
+    assert_equal(S("AooBaB"), s)
+    s[0] = S("ABC")
+    assert_equal(S("ABCooBaB"), s)
+
+    s = S("FooBar")
+    s[0,3] = S("A")
+    assert_equal(S("ABar"),s)
+    s[0] = S("Foo")
+    assert_equal(S("FooBar"), s)
+    s[-3,3] = S("Foo")
+    assert_equal(S("FooFoo"), s)
+    assert_raise(IndexError) { s[7,3] =  S("Bar") }
+    assert_raise(IndexError) { s[-7,3] = S("Bar") }
+
+    s = S("FooBar")
+    s[0..2] = S("A")
+    assert_equal(S("ABar"), s)
+    s[1..3] = S("Foo")
+    assert_equal(S("AFoo"), s)
+    s[-4..-4] = S("Foo")
+    assert_equal(S("FooFoo"), s)
+    assert_raise(RangeError) { s[7..10]   = S("Bar") }
+    assert_raise(RangeError) { s[-7..-10] = S("Bar") }
+
+    s = S("FooBar")
+    s[/^F../]= S("Bar")
+    assert_equal(S("BarBar"), s)
+    s[/..r$/] = S("Foo")
+    assert_equal(S("BarFoo"), s)
+    if @aref_re_silent
+      s[/xyzzy/] = S("None")
+      assert_equal(S("BarFoo"), s)
+    else
+      assert_raise(IndexError) { s[/xyzzy/] = S("None") }
+    end
+    if @aref_re_nth
+      s[/([A-Z]..)([A-Z]..)/, 1] = S("Foo")
+      assert_equal(S("FooFoo"), s)
+      s[/([A-Z]..)([A-Z]..)/, 2] = S("Bar")
+      assert_equal(S("FooBar"), s)
+      assert_raise(IndexError) { s[/([A-Z]..)([A-Z]..)/, 3] = "None" }
+      s[/([A-Z]..)([A-Z]..)/, -1] = S("Foo")
+      assert_equal(S("FooFoo"), s)
+      s[/([A-Z]..)([A-Z]..)/, -2] = S("Bar")
+      assert_equal(S("BarFoo"), s)
+      assert_raise(IndexError) { s[/([A-Z]..)([A-Z]..)/, -3] = "None" }
+    end
+
+    s = S("FooBar")
+    s[S("Foo")] = S("Bar")
+    assert_equal(S("BarBar"), s)
+
+    s = S("a string")
+    s[0..s.size] = S("another string")
+    assert_equal(S("another string"), s)
+
+    o = Object.new
+    def o.to_int; 2; end
+    s = "foo"
+    s[o] = "bar"
+    assert_equal("fobar", s)
+
+    assert_raise(ArgumentError) { "foo"[1, 2, 3] = "" }
+
+    assert_raise(IndexError) {"foo"[RbConfig::LIMITS["LONG_MIN"]] = "l"}
+  end
+
+  def test_CMP # '<=>'
+    assert_equal(1, S("abcdef") <=> S("abcde"))
+    assert_equal(0, S("abcdef") <=> S("abcdef"))
+    assert_equal(-1, S("abcde") <=> S("abcdef"))
+
+    assert_equal(-1, S("ABCDEF") <=> S("abcdef"))
+
+    assert_nil("foo" <=> Object.new)
+
+    o = Object.new
+    def o.to_str; "bar"; end
+    assert_equal(1, "foo" <=> o)
+
+    class << o;remove_method :to_str;end
+    def o.<=>(x); nil; end
+    assert_nil("foo" <=> o)
+
+    class << o;remove_method :<=>;end
+    def o.<=>(x); 1; end
+    assert_equal(-1, "foo" <=> o)
+
+    class << o;remove_method :<=>;end
+    def o.<=>(x); 2**100; end
+    assert_equal(-1, "foo" <=> o)
+  end
+
+  def test_EQUAL # '=='
+    assert_not_equal(:foo, S("foo"))
+    assert_equal(S("abcdef"), S("abcdef"))
+
+    assert_not_equal(S("CAT"), S('cat'))
+    assert_not_equal(S("CaT"), S('cAt'))
+    assert_not_equal(S("cat\0""dog"), S("cat\0"))
+
+    o = Object.new
+    def o.to_str; end
+    def o.==(x); false; end
+    assert_equal(false, "foo" == o)
+    class << o;remove_method :==;end
+    def o.==(x); true; end
+    assert_equal(true, "foo" == o)
+  end
+
+  def test_LSHIFT # '<<'
+    assert_equal(S("world!"), S("world") << 33)
+    assert_equal(S("world!"), S("world") << S("!"))
+
+    s = "a"
+    10.times {|i|
+      s << s
+      assert_equal("a" * (2 << i), s)
+    }
+
+    s = ["foo"].pack("p")
+    l = s.size
+    s << "bar"
+    assert_equal(l + 3, s.size)
+
+    bug = '[ruby-core:27583]'
+    assert_raise(RangeError, bug) {S("a".force_encoding(Encoding::UTF_8)) << -3}
+    assert_raise(RangeError, bug) {S("a".force_encoding(Encoding::UTF_8)) << -2}
+    assert_raise(RangeError, bug) {S("a".force_encoding(Encoding::UTF_8)) << -1}
+    assert_raise(RangeError, bug) {S("a".force_encoding(Encoding::UTF_8)) << 0x81308130}
+    assert_nothing_raised {S("a".force_encoding(Encoding::GB18030)) << 0x81308130}
+  end
+
+  def test_MATCH # '=~'
+    assert_equal(10,  S("FeeFieFoo-Fum") =~ /Fum$/)
+    assert_equal(nil, S("FeeFieFoo-Fum") =~ /FUM$/)
+
+    o = Object.new
+    def o.=~(x); x + "bar"; end
+    assert_equal("foobar", S("foo") =~ o)
+
+    assert_raise(TypeError) { S("foo") =~ "foo" }
+  end
+
+  def test_MOD # '%'
+    assert_equal(S("00123"
