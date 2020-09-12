de57b7d06abeb5583c3ac43f377f9e49038a52fd
seadu <68719109+seadu@users.noreply.github.com>
2020-09-12 21:21:43 +0000

    l10n: Updated Bulgarian (bg) translation to 100%

--- src/main/java/org/truffleruby/parser/ast/VCallParseNode.java
+++ src/main/java/org/truffleruby/parser/ast/VCallParseNode.java
@@ -22,4 +22,54 @@
  * either of the GNU General Public License Version 2 or later (the "GPL"),
  * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of you
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
+ ***** END LICENSE BLOCK *****/
+package org.truffleruby.parser.ast;
+
+import java.util.List;
+
+import org.truffleruby.language.SourceIndexLength;
+import org.truffleruby.parser.ast.types.INameNode;
+import org.truffleruby.parser.ast.visitor.NodeVisitor;
+
+/** RubyMethod call without any arguments */
+public class VCallParseNode extends ParseNode implements INameNode {
+    private String name;
+
+    public VCallParseNode(SourceIndexLength position, String name) {
+        super(position);
+
+        this.name = name;
+    }
+
+    @Override
+    public NodeType getNodeType() {
+        return NodeType.VCALLNODE;
+    }
+
+    /** Accept for the visitor pattern.
+     * 
+     * @param iVisitor the visitor **/
+    @Override
+    public <T> T accept(NodeVisitor<T> iVisitor) {
+        return iVisitor.visitVCallNode(this);
+    }
+
+    /** Gets the methodName.
+     * 
+     * @return Returns a String */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public List<ParseNode> childNodes() {
+        return EMPTY_LIST;
+    }
+}
