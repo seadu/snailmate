8c4af746495c3643428b9b816e0ea6975268e83b
seadu <68719109+seadu@users.noreply.github.com>
2020-09-18 14:13:47 +0000

    Bump dev version

--- src/main/java/org/truffleruby/parser/ast/LambdaParseNode.java
+++ src/main/java/org/truffleruby/parser/ast/LambdaParseNode.java
@@ -16,4 +16,50 @@
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the EPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the EPL, the GPL or the LGPL.
+ ***** END LICENSE BLOCK *****/
+package org.truffleruby.parser.ast;
+
+import java.util.List;
+
+import org.truffleruby.language.SourceIndexLength;
+import org.truffleruby.parser.ast.visitor.NodeVisitor;
+import org.truffleruby.parser.scope.StaticScope;
+
+/** Stubby lambda node (1.9 only) */
+public class LambdaParseNode extends IterParseNode {
+    public LambdaParseNode(SourceIndexLength position, ArgsParseNode args, ParseNode body, StaticScope scope) {
+        super(position, args, body, scope);
+    }
+
+    @Override
+    public NodeType getNodeType() {
+        return NodeType.LAMBDANODE;
+    }
+
+    public ArgsParseNode getArgs() {
+        return (ArgsParseNode) getVarNode();
+    }
+
+    public ParseNode getBody() {
+        return getBodyNode();
+    }
+
+    @Override
+    public <T> T accept(NodeVisitor<T> visitor) {
+        return visitor.visitLambdaNode(this);
+    }
+
+    @Override
+    public List<ParseNode> childNodes() {
+        return ParseNode.createList(getArgs(), getBody());
+    }
+}
