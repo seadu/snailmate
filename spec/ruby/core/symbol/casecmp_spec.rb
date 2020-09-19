bb768f6e33df3b94c11a1932a3a97d934bbb68e9
seadu <68719109+seadu@users.noreply.github.com>
2020-09-19 07:40:28 +0000

    Removing unnecessary comments

--- spec/ruby/core/symbol/casecmp_spec.rb
+++ spec/ruby/core/symbol/casecmp_spec.rb
@@ -95,4 +95,58 @@ describe 'Symbol#casecmp?' do
     lower_a_tilde.casecmp?(lower_a_umlaut).should_not == true
     lower_a_umlaut.casecmp?(lower_a_tilde).should_not == true
     upper_a_tilde.casecmp?(upper_a_umlaut).should_not == true
-    upper_a_umlaut.casecmp?(upper_a_tilde).should_not == t
+    upper_a_umlaut.casecmp?(upper_a_tilde).should_not == true
+
+    # -- UTF-8 --
+    upper_a_tilde  = :"Ã"
+    lower_a_tilde  = :"ã"
+    upper_a_umlaut = :"Ä"
+    lower_a_umlaut = :"ä"
+
+    lower_a_tilde.casecmp?(lower_a_umlaut).should_not == true
+    lower_a_umlaut.casecmp?(lower_a_tilde).should_not == true
+    upper_a_tilde.casecmp?(upper_a_umlaut).should_not == true
+    upper_a_umlaut.casecmp?(upper_a_tilde).should_not == true
+  end
+
+  it "doesn't do case mapping for non-ascii and non-unicode characters" do
+    # -- Latin-1 --
+    upper_a_tilde  = "\xC3".b.to_sym
+    upper_a_umlaut = "\xC4".b.to_sym
+    lower_a_tilde  = "\xE3".b.to_sym
+    lower_a_umlaut = "\xE4".b.to_sym
+
+    upper_a_tilde.casecmp?(lower_a_tilde).should == false
+    upper_a_umlaut.casecmp?(lower_a_umlaut).should == false
+    lower_a_tilde.casecmp?(upper_a_tilde).should == false
+    lower_a_umlaut.casecmp?(upper_a_umlaut).should == false
+  end
+
+  it 'does case mapping for unicode characters' do
+    # -- UTF-8 --
+    upper_a_tilde  = :"Ã"
+    lower_a_tilde  = :"ã"
+    upper_a_umlaut = :"Ä"
+    lower_a_umlaut = :"ä"
+
+    upper_a_tilde.casecmp?(lower_a_tilde).should == true
+    upper_a_umlaut.casecmp?(lower_a_umlaut).should == true
+    lower_a_tilde.casecmp?(upper_a_tilde).should == true
+    lower_a_umlaut.casecmp?(upper_a_umlaut).should == true
+  end
+
+  it 'returns nil when comparing characters with different encodings' do
+    # -- Latin-1 --
+    upper_a_tilde = "\xC3".b.to_sym
+
+    # -- UTF-8 --
+    lower_a_tilde = :"ã"
+
+    upper_a_tilde.casecmp?(lower_a_tilde).should == nil
+    lower_a_tilde.casecmp?(upper_a_tilde).should == nil
+  end
+
+  it "returns true for empty symbols in different encodings" do
+    ''.to_sym.should.casecmp?(''.encode("UTF-32LE").to_sym)
+  end
+end
---
+++ test/mri/tests/ruby/test_continuation.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: false
+require 'test/unit'
+EnvUtil.suppress_warning {require 'continuation'}
+require 'fiber'
+
+class TestContinuation < Test::Unit::TestCase
+  def test_create
+    assert_equal(:ok, callcc{:ok})
+    assert_equal(:ok, callcc{|c| c.call :ok})
+  end
+
+  def test_call
+    assert_equal(:ok, callcc{|c| c.call :ok})
+
+    ary = []
+    ary << callcc{|c|
+      @cont = c
+      :a
+    }
+    @cont.call :b if ary.length < 3
+    assert_equal([:a, :b, :b], ary)
+  end
+
+  def test_check_localvars
+    vv = 0
+    @v = 0
+    @ary = []
+    [1, 2, 3].each{|i|
+      callcc {|k| @k = k}
+      @v += 1
+      vv += 1
+    }
+    @ary << [vv, @v]
+    @k.call if @v < 10
+    assert_equal((3..10).map{|e| [e, e]}, @ary)
+  end
+
+  def test_error
+    cont = callcc{|c| c}
+    Thread.new{
+      assert_raise(RuntimeError){
+        cont.call
+      }
+    }.join
+    assert_raise(LocalJumpError){
+      callcc
+    }
+    assert_raise(RuntimeError){
+      c = nil
+      Fiber.new do
+        callcc {|c2| c = c2 }
+      end.resume
+      c.call
+    }
+  end
+
+  def test_ary_flatten
+    assert_normal_exit %q{
+      require 'continuation'
+   
