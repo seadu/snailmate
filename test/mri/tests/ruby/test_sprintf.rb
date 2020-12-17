7566e294ce9f436032b7aadfb65c26365872bb43
seadu <68719109+seadu@users.noreply.github.com>
2020-12-17 06:18:29 +0000

    Update phpunit.xml

--- test/mri/tests/ruby/test_sprintf.rb
+++ test/mri/tests/ruby/test_sprintf.rb
@@ -197,4 +197,153 @@ class TestSprintf < Test::Unit::TestCase
     assert_equal('..f00000000',
       sprintf("%x", -2**32), '[ruby-dev:32351]')
     assert_equal("..101111111111111111111111111111111",
-      sprintf("%b", -
+      sprintf("%b", -2147483649), '[ruby-dev:32365]')
+    assert_equal(" Inf", sprintf("% e", inf), '[ruby-dev:34002]')
+  end
+
+  def test_bignum
+    assert_match(/\A10{120}\.0+\z/, sprintf("%f", 100**60))
+    assert_match(/\A10{180}\.0+\z/, sprintf("%f", 1000**60))
+  end
+
+  def test_rational
+    assert_match(/\A0\.10+\z/, sprintf("%.60f", 0.1r))
+    assert_match(/\A0\.010+\z/, sprintf("%.60f", 0.01r))
+    assert_match(/\A0\.0010+\z/, sprintf("%.60f", 0.001r))
+    assert_match(/\A0\.3+\z/, sprintf("%.60f", 1/3r))
+    assert_match(/\A1\.20+\z/, sprintf("%.60f", 1.2r))
+
+    ["", *"0".."9"].each do |len|
+      ["", *".0"..".9"].each do |prec|
+        ['', '+', '-', ' ', '0', '+0', '-0', ' 0', '+ ', '- ', '+ 0', '- 0'].each do |flags|
+          fmt = "%#{flags}#{len}#{prec}f"
+          [0, 0.1, 0.01, 0.001, 1.001, 100.0, 100.001, 10000000000.0, 0.00000000001, 1/3r, 2/3r, 1.2r, 10r].each do |num|
+            assert_equal(sprintf(fmt, num.to_f), sprintf(fmt, num.to_r), "sprintf(#{fmt.inspect}, #{num.inspect}.to_r)")
+            assert_equal(sprintf(fmt, -num.to_f), sprintf(fmt, -num.to_r), "sprintf(#{fmt.inspect}, #{(-num).inspect}.to_r)") if num > 0
+          end
+        end
+      end
+    end
+
+    bug11766 = '[ruby-core:71806] [Bug #11766]'
+    assert_equal("x"*10+"     1.0", sprintf("x"*10+"%8.1f", 1r), bug11766)
+  end
+
+  def test_rational_precision
+    assert_match(/\A0\.\d{600}\z/, sprintf("%.600f", 600**~60))
+  end
+
+  def test_hash
+    options = {:capture=>/\d+/}
+    assert_equal("with options {:capture=>/\\d+/}", sprintf("with options %p" % options))
+  end
+
+  def test_inspect
+    obj = Object.new
+    def obj.inspect; "TEST"; end
+    assert_equal("<TEST>", sprintf("<%p>", obj))
+  end
+
+  def test_invalid
+    # Star precision before star width:
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%.**d", 5, 10, 1)}
+
+    # Precision before flags and width:
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%.5+05d", 5)}
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%.5 5d", 5)}
+
+    # Overriding a star width with a numeric one:
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%*1s", 5, 1)}
+
+    # Width before flags:
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%5+0d", 1)}
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%5 0d", 1)}
+
+    # Specifying width multiple times:
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%50+30+20+10+5d", 5)}
+    assert_raise(ArgumentError, "[ruby-core:11569]") {sprintf("%50 30 20 10 5d", 5)}
+
+    # Specifying the precision multiple times with negative star arguments:
+    assert_raise(ArgumentError, "[ruby-core:11570]") {sprintf("%.*.*.*.*f", -1, -1, -1, 5, 1)}
+
+    # Null bytes after percent signs are removed:
+    assert_equal("%\0x hello", sprintf("%\0x hello"), "[ruby-core:11571]")
+
+    assert_raise(ArgumentError, "[ruby-core:11573]") {sprintf("%.25555555555555555555555555555555555555s", "hello")}
+
+    assert_raise(ArgumentError) { sprintf("%\1", 1) }
+    assert_raise(ArgumentError) { sprintf("%!", 1) }
+    assert_raise(ArgumentError) { sprintf("%1$1$d", 1) }
+    assert_raise(ArgumentError) { sprintf("%0%") }
+
+    assert_raise_with_message(ArgumentError, /unnumbered\(1\) mixed with numbered/) { sprintf("%1$*d", 3) }
+    assert_raise_with_message(ArgumentError, /unnumbered\(1\) mixed with numbered/) { sprintf("%1$.*d", 3) }
+
+    verbose, $VERBOSE = $VERBOSE, nil
+    assert_nothing_raised { sprintf("", 1) }
+  ensure
+    $VERBOSE = verbose
+  end
+
+  def test_float
+    assert_equal("36893488147419111424",
+                 sprintf("%20.0f", 36893488147419107329.0))
+    assert_equal(" Inf", sprintf("% 0e", 1.0/0.0), "moved from btest/knownbug")
+    assert_equal("       -0.", sprintf("%#10.0f", -0.5), "[ruby-dev:42552]")
+    # out of spec
+    #assert_equal("0x1p+2",   sprintf('%.0a', Float('0x1.fp+1')),   "[ruby-dev:42551]")
+    #assert_equal("-0x1.0p+2", sprintf('%.1a', Float('-0x1.ffp+1')), "[ruby-dev:42551]")
+  end
+
+  def test_float_hex
+    assert_equal("-0x0p+0", sprintf("%a", -0.0))
+    assert_equal("0x0p+0", sprintf("%a", 0.0))
+    assert_equal("0x1p-1", sprintf("%a", 0.5))
+    assert_equal("0x1p+0", sprintf("%a", 1.0))
+    assert_equal("0x1p+1", sprintf("%a", 2.0))
+    assert_equal("0x1p+10", sprintf("%a", 1024))
+    assert_equal("0x1.23456p+789", sprintf("%a", 3.704450999893983e+237))
+    assert_equal("0x1p-1074", sprintf("%a", 4.9e-324))
+    assert_equal("Inf", sprintf("%e", Float::INFINITY))
+    assert_equal("Inf", sprintf("%E", Float::INFINITY))
+    assert_equal("NaN", sprintf("%e", Float::NAN))
+    assert_equal("NaN", sprintf("%E", Float::NAN))
+
+    assert_equal("   -0x1p+0", sprintf("%10a", -1))
+    assert_equal(" -0x1.8p+0", sprintf("%10a", -1.5))
+    assert_equal(" -0x1.4p+0", sprintf("%10a", -1.25))
+    assert_equal(" -0x1.2p+0", sprintf("%10a", -1.125))
+    assert_equal(" -0x1.1p+0", sprintf("%10a", -1.0625))
+    assert_equal("-0x1.08p+0", sprintf("%10a", -1.03125))
+
+    bug3962 = '[ruby-core:32841]'
+    assert_equal("-0x0001p+0", sprintf("%010a", -1), bug3962)
+    assert_equal("-0x01.8p+0", sprintf("%010a", -1.5), bug3962)
+    assert_equal("-0x01.4p+0", sprintf("%010a", -1.25), bug3962)
+    assert_equal("-0x01.2p+0", sprintf("%010a", -1.125), bug3962)
+    assert_equal("-0x01.1p+0", sprintf("%010a", -1.0625), bug3962)
+    assert_equal("-0x1.08p+0", sprintf("%010a", -1.03125), bug3962)
+
+    bug3964 = '[ruby-core:32848]'
+    assert_equal("0x000000000000000p+0", sprintf("%020a",  0), bug3964)
+    assert_equal("0x000000000000001p+0", sprintf("%020a", 1), bug3964)
+    assert_equal("-0x00000000000001p+0", sprintf("%020a", -1), bug3964)
+    assert_equal("0x00000000000000.p+0", sprintf("%#020a",  0), bug3964)
+
+    bug3965 = '[ruby-dev:42431]'
+    assert_equal("0x1.p+0", sprintf("%#.0a",  1), bug3965)
+    assert_equal("0x00000000000000.p+0", sprintf("%#020a",  0), bug3965)
+    assert_equal("0x0000.0000000000p+0", sprintf("%#020.10a",  0), bug3965)
+
+    bug3979 = '[ruby-dev:42453]'
+    assert_equal("          0x0.000p+0", sprintf("%20.3a",  0), bug3979)
+    assert_equal("          0x1.000p+0", sprintf("%20.3a",  1), bug3979)
+  end
+
+  def test_float_prec
+    # These four tests are intended to test the round half even
+    # behaviour. We have commented out two of these tests because the
+    # numbers used cannot be precisely represented in double precision
+    # floating point, and so the behaviour depends heavily on how you
+    # treat these cases. We format 5.015 to 5.01 because it is
+    # fractionally below 5.015, and we for
