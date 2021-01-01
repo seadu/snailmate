37306f83e25dfd51c7d102028232411b9d928796
seadu <68719109+seadu@users.noreply.github.com>
2021-01-01 08:49:25 +0000

    index file

--- spec/ruby/core/array/to_h_spec.rb
+++ spec/ruby/core/array/to_h_spec.rb
@@ -28,4 +28,58 @@ describe "Array#to_h" do
     -> { [:x].to_h }.should raise_error(TypeError)
   end
 
-  it "raises ArgumentError if an element is not a [ke
+  it "raises ArgumentError if an element is not a [key, value] pair" do
+    -> { [[:x]].to_h }.should raise_error(ArgumentError)
+  end
+
+  it "does not accept arguments" do
+    -> { [].to_h(:a, :b) }.should raise_error(ArgumentError)
+  end
+
+  it "produces a hash that returns nil for a missing element" do
+    [[:a, 1], [:b, 2]].to_h[:c].should be_nil
+  end
+
+  context "with block" do
+    it "converts [key, value] pairs returned by the block to a Hash" do
+      [:a, :b].to_h { |k| [k, k.to_s] }.should == { a: 'a', b: 'b' }
+    end
+
+    it "raises ArgumentError if block returns longer or shorter array" do
+      -> do
+        [:a, :b].to_h { |k| [k, k.to_s, 1] }
+      end.should raise_error(ArgumentError, /wrong array length at 0/)
+
+      -> do
+        [:a, :b].to_h { |k| [k] }
+      end.should raise_error(ArgumentError, /wrong array length at 0/)
+    end
+
+    it "raises TypeError if block returns something other than Array" do
+      -> do
+        [:a, :b].to_h { |k| "not-array" }
+      end.should raise_error(TypeError, /wrong element type String at 0/)
+    end
+
+    it "coerces returned pair to Array with #to_ary" do
+      x = mock('x')
+      x.stub!(:to_ary).and_return([:b, 'b'])
+
+      [:a].to_h { |k| x }.should == { :b => 'b' }
+    end
+
+    it "does not coerce returned pair to Array with #to_a" do
+      x = mock('x')
+      x.stub!(:to_a).and_return([:b, 'b'])
+
+      -> do
+        [:a].to_h { |k| x }
+      end.should raise_error(TypeError, /wrong element type MockObject at 0/)
+    end
+  end
+end
+
+describe "Array#to_h" do
+  @value_to_return = -> e { [e, e.to_s] }
+  it_behaves_like :array_iterable_and_tolerating_size_increasing, :to_h
+end
---
+++ src/main/java/org/truffleruby/core/inlined/InlinedAddNode.java
@@ -0,0 +1,86 @@
+
+/*
+ * Copyright (c) 2017, 2023 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 2.0, or
+ * GNU General Public License version 2, or
+ * GNU Lesser General Public License version 2.1.
+ */
+package org.truffleruby.core.inlined;
+
+import org.truffleruby.RubyLanguage;
+import org.truffleruby.core.numeric.BigIntegerOps;
+import org.truffleruby.core.numeric.FixnumOrBignumNode;
+import org.truffleruby.language.RubyNode;
+import org.truffleruby.language.dispatch.RubyCallNodeParameters;
+
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+/** See {@link org.truffleruby.core.numeric.IntegerNodes.AddNode} and
+ * {@link org.truffleruby.core.numeric.FloatNodes.AddNode} */
+public abstract class InlinedAddNode extends BinaryInlinedOperationNode {
+
+    public InlinedAddNode(RubyLanguage language, RubyCallNodeParameters callNodeParameters) {
+        super(
+                language,
+                callNodeParameters,
+                language.coreMethodAssumptions.integerAddAssumption,
+                language.coreMethodAssumptions.floatAddAssumption);
+    }
+
+    @Specialization(rewriteOn = ArithmeticException.class, assumptions = "assumptions")
+    protected int intAdd(int a, int b) {
+        return Math.addExact(a, b);
+    }
+
+    @Specialization(assumptions = "assumptions")
+    protected long intAddWithOverflow(int a, int b) {
+        return (long) a + (long) b;
+    }
+
+    @Specialization(rewriteOn = ArithmeticException.class, assumptions = "assumptions")
+    protected long longAdd(long a, long b) {
+        return Math.addExact(a, b);
+    }
+
+    @Specialization(assumptions = "assumptions")
+    protected Object longAddWithOverflow(long a, long b,
+            @Cached FixnumOrBignumNode fixnumOrBignum) {
+        return fixnumOrBignum.fixnumOrBignum(BigIntegerOps.add(a, b));
+    }
+
+    @Specialization(assumptions = "assumptions")
+    protected double floatAdd(double a, double b) {
+        return a + b;
+    }
+
+    @Specialization(assumptions = "assumptions")
+    protected double longDouble(long a, double b) {
+        return a + b;
+    }
+
+    @Specialization(assumptions = "assumptions")
+    protected double doubleLong(double a, long b) {
+        return a + b;
+    }
+
+    @Specialization
+    protected Object fallback(VirtualFrame frame, Object a, Object b) {
+        return rewriteAndCall(frame, a, b);
+    }
+
+    @Override
+    public RubyNode cloneUninitialized() {
+        var copy = InlinedAddNodeGen.create(
+                getLanguage(),
+                this.parameters,
+                getLeftNode().cloneUninitialized(),
+                getRightNode().cloneUninitialized());
+        return copy.copyFlags(this);
+    }
+
+}
