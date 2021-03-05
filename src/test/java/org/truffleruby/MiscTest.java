e7ec99debaa162e698f4735896489dfbb18dd4f4
seadu <68719109+seadu@users.noreply.github.com>
2021-03-05 14:37:18 +0000

    l10n: Updated Galician (gl) translation to 100%

--- src/test/java/org/truffleruby/MiscTest.java
+++ src/test/java/org/truffleruby/MiscTest.java
@@ -55,4 +55,116 @@ public class MiscTest {
             } catch (PolyglotException e) {
                 if (e.isCancelled()) {
                     assertTrue(e.isCancelled());
-            
+                } else {
+                    throw e;
+                }
+            }
+        });
+
+        context.eval("ruby", "init = 1");
+        thread.start();
+        try {
+            String maliciousCode = "while true; end";
+            context.eval("ruby", maliciousCode);
+            Assert.fail();
+        } catch (PolyglotException e) {
+            assertTrue(e.isCancelled());
+        } finally {
+            thread.join();
+        }
+    }
+
+    @Test
+    public void testEvalFromIntegratorThreadSingleThreaded() throws Throwable {
+        final String codeDependingOnCurrentThread = "Thread.current.object_id";
+
+        try (Context context = RubyTest.createContext()) {
+            long thread1 = context.eval("ruby", codeDependingOnCurrentThread).asLong();
+
+            TestingThread thread = new TestingThread(() -> {
+                long thread2 = context.eval("ruby", codeDependingOnCurrentThread).asLong();
+                assertNotEquals(thread1, thread2);
+            });
+            thread.start();
+            thread.join();
+        }
+    }
+
+    @Test
+    public void testFiberFromIntegratorThread() throws Throwable {
+        try (Context context = RubyTest.createContext()) {
+            context.eval("ruby", ":init");
+
+            TestingThread thread = new TestingThread(() -> {
+                int value = context.eval("ruby", "Fiber.new { 6 * 7 }.resume").asInt();
+                assertEquals(42, value);
+            });
+            thread.start();
+            thread.join();
+        }
+    }
+
+    @Test
+    public void testIntegratorThreadRubyThreadInitialization() throws Throwable {
+        try (Context context = Context.newBuilder("ruby").allowCreateThread(true).build()) {
+            context.initialize("ruby");
+            TestingThread thread = new TestingThread(() -> {
+                // Run code that requires the Ruby Thread object to be initialized
+                Value recursiveArray = context.eval("ruby", "a = [0]; a[0] = a; a.inspect"); // Access @recursive_objects
+                assertEquals("[[...]]", recursiveArray.asString());
+                assertTrue(context.eval("ruby", "Thread.current.thread_variable_get('foo')").isNull());
+                assertTrue(context.eval("ruby", "rand").fitsInDouble());
+            });
+            thread.start();
+            thread.join();
+        }
+    }
+
+    @Test
+    public void testForeignThread() throws Throwable {
+        // Tests thread initialization and finalization for a foreign thread and an embedder thread
+        try (Context context = Context.newBuilder("ruby").allowCreateThread(true).build()) {
+            context.eval("ruby", "$queue = Queue.new");
+            assertEquals(context.eval("ruby", "Thread.main"), context.eval("ruby", "Thread.current"));
+            assertTrue(context.eval("ruby", "Thread.current").toString().contains("main"));
+            assertFalse(context.eval("ruby", "Thread.current").toString().contains("<foreign thread>"));
+
+            Runnable polyglotThreadBody = () -> {
+                context.eval("ruby", "$queue.pop");
+                assertTrue(context.eval("ruby", "Thread.current").toString().contains("<foreign thread>"));
+                assertTrue(context.eval("ruby", "Thread.current.equal?(Thread.current)").asBoolean());
+            };
+            Thread polyglotThread = context
+                    .eval("ruby", "Truffle::Debug")
+                    .invokeMember("create_polyglot_thread", polyglotThreadBody)
+                    .asHostObject();
+
+            TestingThread embedderThread = new TestingThread(() -> {
+                context.eval("ruby", "$queue.pop");
+                assertTrue(context.eval("ruby", "Thread.current").toString().contains("<foreign thread>"));
+            });
+
+            polyglotThread.start();
+            embedderThread.start();
+
+            context.eval("ruby", "$queue << 1");
+            context.eval("ruby", "$queue << 2");
+
+            polyglotThread.join(); // causes disposeThread(polyglotThread) to run on polyglotThread
+            embedderThread.join();
+        }
+    }
+
+    @Test
+    public void testIntegratorThreadContextClosedOnOtherThread() throws Throwable {
+        try (Context context = Context.newBuilder("ruby").allowCreateThread(true).build()) {
+            TestingThread thread = new TestingThread(() -> {
+                assertEquals(context.eval("ruby", "Thread.current"), context.eval("ruby", "Thread.main"));
+                assertEquals(42, context.eval("ruby", "6 * 7").asInt());
+            });
+            thread.start();
+            thread.join();
+        }
+    }
+
+}
