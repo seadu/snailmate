0213508e4dc2c8103d6eec06d546d88f4da551db
seadu <68719109+seadu@users.noreply.github.com>
2021-03-16 08:46:53 +0000

    iana portlist updated.

--- spec/ruby/core/kernel/instance_variables_spec.rb
+++ spec/ruby/core/kernel/instance_variables_spec.rb
@@ -11,4 +11,30 @@ describe "Kernel#instance_variables" do
 
     it "returns the correct array if an instance variable is added" do
       a = 0
-      ->{ a.instance_variable_set("@test", 1) }.should r
+      ->{ a.instance_variable_set("@test", 1) }.should raise_error(RuntimeError)
+    end
+  end
+
+  describe "regular objects" do
+    it "returns an empty array if no instance variables are defined" do
+      Object.new.instance_variables.should == []
+    end
+
+    it "returns the correct array if an instance variable is added" do
+      a = Object.new
+      a.instance_variable_set("@test", 1)
+      a.instance_variables.should == [:@test]
+    end
+
+    it "returns the instances variables in the order declared" do
+      c = Class.new do
+        def initialize
+          @c = 1
+          @a = 2
+          @b = 3
+        end
+      end
+      c.new.instance_variables.should == [:@c, :@a, :@b]
+    end
+  end
+end
