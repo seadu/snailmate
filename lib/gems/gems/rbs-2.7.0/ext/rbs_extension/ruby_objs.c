d76440d2e5514e00cac9ecbb0751f0487fe520bc
seadu <68719109+seadu@users.noreply.github.com>
2021-03-18 13:35:55 +0000

    .travis.yml

--- lib/gems/gems/rbs-2.7.0/ext/rbs_extension/ruby_objs.c
+++ lib/gems/gems/rbs-2.7.0/ext/rbs_extension/ruby_objs.c
@@ -160,4 +160,220 @@ VALUE rbs_block(VALUE type, VALUE required, VALUE self_type) {
 VALUE rbs_function_param(VALUE type, VALUE name, VALUE location) {
   VALUE args = rb_hash_new();
   rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
-  rb_hash_ase
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Function_Param,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_function(
+  VALUE required_positional_params,
+  VALUE optional_positional_params,
+  VALUE rest_positional_param,
+  VALUE trailing_positional_params,
+  VALUE required_keyword_params,
+  VALUE optional_keyword_params,
+  VALUE rest_keyword_param,
+  VALUE return_type
+) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("required_positionals")), required_positional_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("optional_positionals")), optional_positional_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("rest_positionals")), rest_positional_param);
+  rb_hash_aset(args, ID2SYM(rb_intern("trailing_positionals")), trailing_positional_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("required_keywords")), required_keyword_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("optional_keywords")), optional_keyword_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("rest_keywords")), rest_keyword_param);
+  rb_hash_aset(args, ID2SYM(rb_intern("return_type")), return_type);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Function,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_proc(VALUE function, VALUE block, VALUE location, VALUE self_type) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), function);
+  rb_hash_aset(args, ID2SYM(rb_intern("block")), block);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("self_type")), self_type);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Proc,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_void(VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Bases_Void,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_literal(VALUE literal, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("literal")), literal);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Literal,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_record(VALUE fields, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("fields")), fields);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Record,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_variable(VALUE name, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_Types_Variable,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_method_type(VALUE type_params, VALUE type, VALUE block, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("type_params")), type_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
+  rb_hash_aset(args, ID2SYM(rb_intern("block")), block);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_MethodType,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_comment(VALUE string, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("string")), string);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_AST_Comment,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_annotation(VALUE string, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("string")), string);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_AST_Annotation,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_type_param(VALUE name, VALUE variance, bool unchecked, VALUE upper_bound, VALUE location) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("variance")), variance);
+  rb_hash_aset(args, ID2SYM(rb_intern("upper_bound")), upper_bound);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+
+  VALUE type_param = CLASS_NEW_INSTANCE(RBS_AST_TypeParam, 1, &args);
+
+  if (unchecked) {
+    rb_funcall(type_param, rb_intern("unchecked!"), 0);
+  }
+
+  return type_param;
+}
+
+VALUE rbs_ast_decl_constant(VALUE name, VALUE type, VALUE location, VALUE comment) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_AST_Declarations_Constant,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_decl_global(VALUE name, VALUE type, VALUE location, VALUE comment) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_AST_Declarations_Global,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_decl_alias(VALUE name, VALUE type_params, VALUE type, VALUE annotations, VALUE location, VALUE comment) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("type_params")), type_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("type")), type);
+  rb_hash_aset(args, ID2SYM(rb_intern("annotations")), annotations);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_AST_Declarations_Alias,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_decl_interface(VALUE name, VALUE type_params, VALUE members, VALUE annotations, VALUE location, VALUE comment) {
+  VALUE args = rb_hash_new();
+  rb_hash_aset(args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(args, ID2SYM(rb_intern("type_params")), type_params);
+  rb_hash_aset(args, ID2SYM(rb_intern("members")), members);
+  rb_hash_aset(args, ID2SYM(rb_intern("annotations")), annotations);
+  rb_hash_aset(args, ID2SYM(rb_intern("location")), location);
+  rb_hash_aset(args, ID2SYM(rb_intern("comment")), comment);
+
+  return CLASS_NEW_INSTANCE(
+    RBS_AST_Declarations_Interface,
+    1,
+    &args
+  );
+}
+
+VALUE rbs_ast_decl_module_self(VALUE name, VALUE args, VALUE location) {
+  VALUE kw_args = rb_hash_new();
+  rb_hash_aset(kw_args, ID2SYM(rb_intern("name")), name);
+  rb_hash_aset(kw_args, ID2SYM(rb_intern("args")), args);
+  rb_hash_aset(kw_args, ID2SYM(rb_intern("location")), l
