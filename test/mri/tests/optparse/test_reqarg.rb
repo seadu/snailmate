fc6fe69747fd71a727e12ae95113d26db67c9996
seadu <68719109+seadu@users.noreply.github.com>
2021-03-23 16:19:00 +0000

    add tail

--- test/mri/tests/optparse/test_reqarg.rb
+++ test/mri/tests/optparse/test_reqarg.rb
@@ -54,4 +54,42 @@ module TestOptionParserReqArg
     assert_raise(OptionParser::MissingArgument) {@opt.parse!(%w"-o")}
     assert_equal(%w"", no_error {@opt.parse!(%w"-o foo")})
     assert_equal("foo", @flag)
-    assert_equal(%w"
+    assert_equal(%w"", no_error {@opt.parse!(%w"-obar")})
+    assert_equal("bar", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"-o=")})
+    assert_equal("=", @flag)
+  end
+
+  def test_long
+    assert_raise(OptionParser::MissingArgument) {@opt.parse!(%w"--opt")}
+    assert_equal(%w"", no_error {@opt.parse!(%w"--opt foo")})
+    assert_equal("foo", @flag)
+    assert_equal(%w"foo", no_error {@opt.parse!(%w"--opt= foo")})
+    assert_equal("", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--opt=foo")})
+    assert_equal("foo", @flag)
+  end
+
+  def test_hyphenize
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with_underscore foo1")})
+    assert_equal("foo1", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with-underscore foo2")})
+    assert_equal("foo2", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with-hyphen foo3")})
+    assert_equal("foo3", @flag)
+    assert_equal(%w"", no_error {@opt.parse!(%w"--with_hyphen foo4")})
+    assert_equal("foo4", @flag)
+  end
+
+  class TestOptionParser::WithPattern < TestOptionParser
+    def test_pattern
+      pat = num = nil
+      @opt.def_option("--pattern=VAL", /(\w+)(?:\s*:\s*(\w+))?/) {|x, y, z| pat = [x, y, z]}
+      @opt.def_option("-T NUM", /\A[1-4]\z/) {|n| num = n}
+      no_error {@opt.parse!(%w"--pattern=key:val")}
+      assert_equal(%w"key:val key val", pat, '[ruby-list:45645]')
+      no_error {@opt.parse!(%w"-T 4")}
+      assert_equal("4", num, '[ruby-dev:37514]')
+    end
+  end
+end
