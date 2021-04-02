19600e9cdd529b0f27b113f37829848f44020d36
seadu <68719109+seadu@users.noreply.github.com>
2021-04-02 16:38:28 +0000

    fix bracket

--- bench/psd.rb/psd.rb/lib/psd/renderer/compose.rb
+++ bench/psd.rb/psd.rb/lib/psd/renderer/compose.rb
@@ -138,4 +138,198 @@ class PSD
       rgba(new_r, new_g, new_b, dst_alpha)
     end
 
-    def lin
+    def linear_dodge(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      new_r = blend_channel(r(bg), (r(bg) + r(fg)) > 255 ? 255 : r(bg) + r(fg), mix_alpha)
+      new_g = blend_channel(g(bg), (g(bg) + g(fg)) > 255 ? 255 : g(bg) + g(fg), mix_alpha)
+      new_b = blend_channel(b(bg), (b(bg) + b(fg)) > 255 ? 255 : b(bg) + b(fg), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+
+    #
+    # Contrasting blend modes
+    #
+
+    def overlay(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      calculate_foreground = Proc.new do |b, f|
+        if b < 128
+          b * f >> 7
+        else
+          255 - ((255 - b) * (255 - f) >> 7)
+        end
+      end
+
+      new_r = blend_channel(r(bg), calculate_foreground.call(r(bg), r(fg)), mix_alpha)
+      new_g = blend_channel(g(bg), calculate_foreground.call(g(bg), g(fg)), mix_alpha)
+      new_b = blend_channel(b(bg), calculate_foreground.call(b(bg), b(fg)), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def soft_light(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      calculate_foreground = Proc.new do |b, f|
+        c1 = b * f >> 8
+        c2 = 255 - ((255 - b) * (255 - f) >> 8)
+        ((255 - b) * c1 >> 8) + (b * c2 >> 8)
+      end
+
+      new_r = blend_channel(r(bg), calculate_foreground.call(r(bg), r(fg)), mix_alpha)
+      new_g = blend_channel(g(bg), calculate_foreground.call(g(bg), g(fg)), mix_alpha)
+      new_b = blend_channel(b(bg), calculate_foreground.call(b(bg), b(fg)), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def hard_light(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      calculate_foreground = Proc.new do |b, f|
+        if f < 128
+          b * f >> 7
+        else
+          255 - ((255 - f) * (255 - b) >> 7)
+        end
+      end
+
+      new_r = blend_channel(r(bg), calculate_foreground.call(r(bg), r(fg)), mix_alpha)
+      new_g = blend_channel(g(bg), calculate_foreground.call(g(bg), g(fg)), mix_alpha)
+      new_b = blend_channel(b(bg), calculate_foreground.call(b(bg), b(fg)), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def vivid_light(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      calculate_foreground = Proc.new do |b, f|
+        if f < 255
+          [(b * b / (255 - f) + f * f / (255 - b)) >> 1, 255].min
+        else
+          b
+        end
+      end
+
+      new_r = blend_channel(r(bg), calculate_foreground.call(r(bg), r(fg)), mix_alpha)
+      new_g = blend_channel(g(bg), calculate_foreground.call(g(bg), g(fg)), mix_alpha)
+      new_b = blend_channel(b(bg), calculate_foreground.call(b(bg), b(fg)), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def linear_light(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      calculate_foreground = Proc.new do |b, f|
+        if b < 255
+          [f * f / (255 - b), 255].min
+        else
+          255
+        end
+      end
+
+      new_r = blend_channel(r(bg), calculate_foreground.call(r(bg), r(fg)), mix_alpha)
+      new_g = blend_channel(g(bg), calculate_foreground.call(g(bg), g(fg)), mix_alpha)
+      new_b = blend_channel(b(bg), calculate_foreground.call(b(bg), b(fg)), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def pin_light(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      calculate_foreground = Proc.new do |b, f|
+        if f >= 128
+          [b, (f - 128) * 2].max
+        else
+          [b, f * 2].min
+        end
+      end
+
+      new_r = blend_channel(r(bg), calculate_foreground.call(r(bg), r(fg)), mix_alpha)
+      new_g = blend_channel(g(bg), calculate_foreground.call(g(bg), g(fg)), mix_alpha)
+      new_b = blend_channel(b(bg), calculate_foreground.call(b(bg), b(fg)), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def hard_mix(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      new_r = blend_channel(r(bg), (r(bg) + r(fg) <= 255) ? 0 : 255, mix_alpha)
+      new_g = blend_channel(g(bg), (g(bg) + g(fg) <= 255) ? 0 : 255, mix_alpha)
+      new_b = blend_channel(b(bg), (b(bg) + b(fg) <= 255) ? 0 : 255, mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    #
+    # Inversion blend modes
+    #
+
+    def difference(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      new_r = blend_channel(r(bg), (r(bg) - r(fg)).abs, mix_alpha)
+      new_g = blend_channel(g(bg), (g(bg) - g(fg)).abs, mix_alpha)
+      new_b = blend_channel(b(bg), (b(bg) - b(fg)).abs, mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    def exclusion(fg, bg, opts={})
+      return apply_opacity(fg, opts) if fully_transparent?(bg)
+      return bg if fully_transparent?(fg)
+
+      mix_alpha, dst_alpha = calculate_alphas(fg, bg, DEFAULT_OPTS.merge(opts))
+
+      new_r = blend_channel(r(bg), r(bg) + r(fg) - (r(bg) * r(fg) >> 7), mix_alpha)
+      new_g = blend_channel(g(bg), g(bg) + g(fg) - (g(bg) * g(fg) >> 7), mix_alpha)
+      new_b = blend_channel(b(bg), b(bg) + b(fg) - (b(bg) * b(fg) >> 7), mix_alpha)
+
+      rgba(new_r, new_g, new_b, dst_alpha)
+    end
+
+    # If the blend mode is missing, we fall back to normal composition.
+    def method_missing(method, *args, &block)
+      return ChunkyPNG::Color.send(method, *args) if ChunkyPNG::Color.respond_to?(method)
+      normal(*args)
+    end
+
+    private
+
+    def calculate_alphas(fg, bg, opts)
+      opacity = calculate_opacity(op
