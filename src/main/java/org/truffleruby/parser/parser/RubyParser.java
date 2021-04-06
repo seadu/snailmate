46856ee2023029741c76e733a468009e3aa25e73
seadu <68719109+seadu@users.noreply.github.com>
2021-04-06 17:20:23 +0000

    add google site verification

--- src/main/java/org/truffleruby/parser/parser/RubyParser.java
+++ src/main/java/org/truffleruby/parser/parser/RubyParser.java
@@ -163,4 +163,156 @@ public class RubyParser {
   public static final int keyword_module = 258;
   public static final int keyword_def = 259;
   public static final int keyword_undef = 260;
-  public static fin
+  public static final int keyword_begin = 261;
+  public static final int keyword_rescue = 262;
+  public static final int keyword_ensure = 263;
+  public static final int keyword_end = 264;
+  public static final int keyword_if = 265;
+  public static final int keyword_unless = 266;
+  public static final int keyword_then = 267;
+  public static final int keyword_elsif = 268;
+  public static final int keyword_else = 269;
+  public static final int keyword_case = 270;
+  public static final int keyword_when = 271;
+  public static final int keyword_while = 272;
+  public static final int keyword_until = 273;
+  public static final int keyword_for = 274;
+  public static final int keyword_break = 275;
+  public static final int keyword_next = 276;
+  public static final int keyword_redo = 277;
+  public static final int keyword_retry = 278;
+  public static final int keyword_in = 279;
+  public static final int keyword_do = 280;
+  public static final int keyword_do_cond = 281;
+  public static final int keyword_do_block = 282;
+  public static final int keyword_return = 283;
+  public static final int keyword_yield = 284;
+  public static final int keyword_super = 285;
+  public static final int keyword_self = 286;
+  public static final int keyword_nil = 287;
+  public static final int keyword_true = 288;
+  public static final int keyword_false = 289;
+  public static final int keyword_and = 290;
+  public static final int keyword_or = 291;
+  public static final int keyword_not = 292;
+  public static final int modifier_if = 293;
+  public static final int modifier_unless = 294;
+  public static final int modifier_while = 295;
+  public static final int modifier_until = 296;
+  public static final int modifier_rescue = 297;
+  public static final int keyword_alias = 298;
+  public static final int keyword_defined = 299;
+  public static final int keyword_BEGIN = 300;
+  public static final int keyword_END = 301;
+  public static final int keyword__LINE__ = 302;
+  public static final int keyword__FILE__ = 303;
+  public static final int keyword__ENCODING__ = 304;
+  public static final int keyword_do_lambda = 305;
+  public static final int tIDENTIFIER = 306;
+  public static final int tFID = 307;
+  public static final int tGVAR = 308;
+  public static final int tIVAR = 309;
+  public static final int tCONSTANT = 310;
+  public static final int tCVAR = 311;
+  public static final int tLABEL = 312;
+  public static final int tCHAR = 313;
+  public static final int tUPLUS = 314;
+  public static final int tUMINUS = 315;
+  public static final int tUMINUS_NUM = 316;
+  public static final int tPOW = 317;
+  public static final int tCMP = 318;
+  public static final int tEQ = 319;
+  public static final int tEQQ = 320;
+  public static final int tNEQ = 321;
+  public static final int tGEQ = 322;
+  public static final int tLEQ = 323;
+  public static final int tANDOP = 324;
+  public static final int tOROP = 325;
+  public static final int tMATCH = 326;
+  public static final int tNMATCH = 327;
+  public static final int tDOT = 328;
+  public static final int tDOT2 = 329;
+  public static final int tDOT3 = 330;
+  public static final int tBDOT2 = 331;
+  public static final int tBDOT3 = 332;
+  public static final int tAREF = 333;
+  public static final int tASET = 334;
+  public static final int tLSHFT = 335;
+  public static final int tRSHFT = 336;
+  public static final int tANDDOT = 337;
+  public static final int tCOLON2 = 338;
+  public static final int tCOLON3 = 339;
+  public static final int tOP_ASGN = 340;
+  public static final int tASSOC = 341;
+  public static final int tLPAREN = 342;
+  public static final int tLPAREN2 = 343;
+  public static final int tRPAREN = 344;
+  public static final int tLPAREN_ARG = 345;
+  public static final int tLBRACK = 346;
+  public static final int tRBRACK = 347;
+  public static final int tLBRACE = 348;
+  public static final int tLBRACE_ARG = 349;
+  public static final int tSTAR = 350;
+  public static final int tSTAR2 = 351;
+  public static final int tAMPER = 352;
+  public static final int tAMPER2 = 353;
+  public static final int tTILDE = 354;
+  public static final int tPERCENT = 355;
+  public static final int tDIVIDE = 356;
+  public static final int tPLUS = 357;
+  public static final int tMINUS = 358;
+  public static final int tLT = 359;
+  public static final int tGT = 360;
+  public static final int tPIPE = 361;
+  public static final int tBANG = 362;
+  public static final int tCARET = 363;
+  public static final int tLCURLY = 364;
+  public static final int tRCURLY = 365;
+  public static final int tBACK_REF2 = 366;
+  public static final int tSYMBEG = 367;
+  public static final int tSTRING_BEG = 368;
+  public static final int tXSTRING_BEG = 369;
+  public static final int tREGEXP_BEG = 370;
+  public static final int tWORDS_BEG = 371;
+  public static final int tQWORDS_BEG = 372;
+  public static final int tSTRING_DBEG = 373;
+  public static final int tSTRING_DVAR = 374;
+  public static final int tSTRING_END = 375;
+  public static final int tLAMBDA = 376;
+  public static final int tLAMBEG = 377;
+  public static final int tNTH_REF = 378;
+  public static final int tBACK_REF = 379;
+  public static final int tSTRING_CONTENT = 380;
+  public static final int tINTEGER = 381;
+  public static final int tIMAGINARY = 382;
+  public static final int tFLOAT = 383;
+  public static final int tRATIONAL = 384;
+  public static final int tREGEXP_END = 385;
+  public static final int tSYMBOLS_BEG = 386;
+  public static final int tQSYMBOLS_BEG = 387;
+  public static final int tDSTAR = 388;
+  public static final int tSTRING_DEND = 389;
+  public static final int tLABEL_END = 390;
+  public static final int tLOWEST = 391;
+  public static final int yyErrorCode = 256;
+
+  /** number of final state.
+    */
+  protected static final int yyFinal = 1;
+
+  /** parser tables.
+      Order is mandated by <i>jay</i>.
+    */
+  protected static final short[] yyLhs = {
+//yyLhs 675
+    -1,   156,     0,   142,   143,   143,   143,   143,   144,   144,
+    37,    36,    38,    38,    38,    38,    44,   159,    44,   160,
+    39,    39,    39,    39,    39,    39,    39,    39,    39,    39,
+    39,    39,    39,    39,    39,    39,    39,    31,    31,    31,
+    31,    31,    31,    31,    31,    62,    62,    62,    40,    40,
+    40,    40,    40,    40,    45,    32,    32,    61,    61,   116,
+   152,    43,    43,    43,    43,    43,    43,    43,    43,    43,
+    43,    43,   119,   119,   130,   130,   120,   120,   120,   120,
+   120,   120,   120,   120,   120,   120,    76,    76,   106,   106,
+   107,   107,    77,    77,    77,    77,    77,    77,    77,    77,
+    77,    77,    77,    77,    77,    77, 
