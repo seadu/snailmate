f170fee42d56e6279f4cd6e7eaf0b89d200433ac
seadu <68719109+seadu@users.noreply.github.com>
2021-04-06 09:56:06 +0000

    sigtimedwait: use freezable blocking call

--- src/main/java/org/truffleruby/parser/parser/RubyParser.java
+++ src/main/java/org/truffleruby/parser/parser/RubyParser.java
@@ -26,4 +26,141 @@
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the EPL, the GPL or the LGPL.
  *
- * Copyright (c) 2016 O
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 2.0, or
+ * GNU General Public License version 2, or
+ * GNU Lesser General Public License version 2.1.
+ ***** END LICENSE BLOCK *****/
+// created by jay 1.0.2 (c) 2002-2004 ats@cs.rit.edu
+// skeleton Java 1.0 (c) 2002 ats@cs.rit.edu
+
+// line 2 "RubyParser.y"
+package org.truffleruby.parser.parser;
+
+import com.oracle.truffle.api.strings.TruffleString;
+
+import org.truffleruby.Layouts;
+import org.truffleruby.annotations.SuppressFBWarnings;
+import org.truffleruby.core.encoding.Encodings;
+import org.truffleruby.core.encoding.RubyEncoding;
+import org.truffleruby.core.encoding.TStringUtils;
+import org.truffleruby.core.string.TStringConstants;
+import org.truffleruby.language.SourceIndexLength;
+import org.truffleruby.parser.RubyDeferredWarnings;
+import org.truffleruby.parser.ast.ArgsParseNode;
+import org.truffleruby.parser.ast.ArgumentParseNode;
+import org.truffleruby.parser.ast.ArrayParseNode;
+import org.truffleruby.parser.ast.AssignableParseNode;
+import org.truffleruby.parser.ast.BackRefParseNode;
+import org.truffleruby.parser.ast.BeginParseNode;
+import org.truffleruby.parser.ast.BlockAcceptingParseNode;
+import org.truffleruby.parser.ast.BlockArgParseNode;
+import org.truffleruby.parser.ast.BlockParseNode;
+import org.truffleruby.parser.ast.BlockPassParseNode;
+import org.truffleruby.parser.ast.BreakParseNode;
+import org.truffleruby.parser.ast.ClassParseNode;
+import org.truffleruby.parser.ast.ClassVarAsgnParseNode;
+import org.truffleruby.parser.ast.ClassVarParseNode;
+import org.truffleruby.parser.ast.Colon3ParseNode;
+import org.truffleruby.parser.ast.ConstDeclParseNode;
+import org.truffleruby.parser.ast.ConstParseNode;
+import org.truffleruby.parser.ast.DStrParseNode;
+import org.truffleruby.parser.ast.DSymbolParseNode;
+import org.truffleruby.parser.ast.DXStrParseNode;
+import org.truffleruby.parser.ast.DefnParseNode;
+import org.truffleruby.parser.ast.DefsParseNode;
+import org.truffleruby.parser.ast.DotParseNode;
+import org.truffleruby.parser.ast.EncodingParseNode;
+import org.truffleruby.parser.ast.EnsureParseNode;
+import org.truffleruby.parser.ast.EvStrParseNode;
+import org.truffleruby.parser.ast.FCallParseNode;
+import org.truffleruby.parser.ast.FalseParseNode;
+import org.truffleruby.parser.ast.FileParseNode;
+import org.truffleruby.parser.ast.FixnumParseNode;
+import org.truffleruby.parser.ast.FloatParseNode;
+import org.truffleruby.parser.ast.ForParseNode;
+import org.truffleruby.parser.ast.GlobalAsgnParseNode;
+import org.truffleruby.parser.ast.GlobalVarParseNode;
+import org.truffleruby.parser.ast.HashParseNode;
+import org.truffleruby.parser.ast.IfParseNode;
+import org.truffleruby.parser.ast.InstAsgnParseNode;
+import org.truffleruby.parser.ast.InstVarParseNode;
+import org.truffleruby.parser.ast.IterParseNode;
+import org.truffleruby.parser.ast.LambdaParseNode;
+import org.truffleruby.parser.ast.ListParseNode;
+import org.truffleruby.parser.ast.LiteralParseNode;
+import org.truffleruby.parser.ast.LocalVarParseNode;
+import org.truffleruby.parser.ast.ModuleParseNode;
+import org.truffleruby.parser.ast.MultipleAsgnParseNode;
+import org.truffleruby.parser.ast.NextParseNode;
+import org.truffleruby.parser.ast.NilImplicitParseNode;
+import org.truffleruby.parser.ast.NilParseNode;
+import org.truffleruby.parser.ast.NonLocalControlFlowParseNode;
+import org.truffleruby.parser.ast.NumericParseNode;
+import org.truffleruby.parser.ast.OpAsgnAndParseNode;
+import org.truffleruby.parser.ast.OpAsgnOrParseNode;
+import org.truffleruby.parser.ast.OptArgParseNode;
+import org.truffleruby.parser.ast.ParseNode;
+import org.truffleruby.parser.ast.PostExeParseNode;
+import org.truffleruby.parser.ast.PreExe19ParseNode;
+import org.truffleruby.parser.ast.RationalParseNode;
+import org.truffleruby.parser.ast.RedoParseNode;
+import org.truffleruby.parser.ast.RegexpParseNode;
+import org.truffleruby.parser.ast.RequiredKeywordArgumentValueParseNode;
+import org.truffleruby.parser.ast.RescueBodyParseNode;
+import org.truffleruby.parser.ast.RescueParseNode;
+import org.truffleruby.parser.ast.RestArgParseNode;
+import org.truffleruby.parser.ast.RetryParseNode;
+import org.truffleruby.parser.ast.ReturnParseNode;
+import org.truffleruby.parser.ast.SClassParseNode;
+import org.truffleruby.parser.ast.SelfParseNode;
+import org.truffleruby.parser.ast.SplatParseNode;
+import org.truffleruby.parser.ast.StarParseNode;
+import org.truffleruby.parser.ast.StrParseNode;
+import org.truffleruby.parser.ast.TrueParseNode;
+import org.truffleruby.parser.ast.UnnamedRestArgParseNode;
+import org.truffleruby.parser.ast.UntilParseNode;
+import org.truffleruby.parser.ast.VAliasParseNode;
+import org.truffleruby.parser.ast.WhileParseNode;
+import org.truffleruby.parser.ast.XStrParseNode;
+import org.truffleruby.parser.ast.YieldParseNode;
+import org.truffleruby.parser.ast.ZArrayParseNode;
+import org.truffleruby.parser.ast.ZSuperParseNode;
+import org.truffleruby.parser.ast.types.ILiteralNode;
+import org.truffleruby.parser.lexer.LexerSource;
+import org.truffleruby.parser.lexer.RubyLexer;
+import org.truffleruby.parser.lexer.StrTerm;
+import org.truffleruby.parser.lexer.SyntaxException.PID;
+
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_BEG;
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_END;
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_ENDARG;
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_ENDFN;
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_FITEM;
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_FNAME;
+import static org.truffleruby.parser.lexer.RubyLexer.EXPR_LABEL;
+import static org.truffleruby.parser.parser.ParserSupport.value_expr;
+
+// @formatter:off
+// CheckStyle: start generated
+@SuppressFBWarnings("IP")
+@SuppressWarnings({"unchecked", "fallthrough", "cast"})
+public class RubyParser {
+    protected final ParserSupport support;
+    protected final RubyLexer lexer;
+
+    public RubyParser(LexerSource source, RubyDeferredWarnings warnings) {
+        this.support = new ParserSupport(source, warnings);
+        this.lexer = new RubyLexer(support, source, warnings);
+        support.setLexer(lexer);
+    }
+// line 125 "-"
+  // %token constants
+  public static final int keyword_class = 257;
+  public static final int keyword_module = 258;
+  public static final int keyword_def = 259;
+  public static final int keyword_undef = 260;
+  public static fin
