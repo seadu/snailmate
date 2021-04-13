83a1f911ec7484b54fd9a2ec54f59e3703464d6b
seadu <68719109+seadu@users.noreply.github.com>
2021-04-13 15:19:39 +0000

    Add google analytics tracking code

--- src/main/java/org/truffleruby/parser/parser/RubyParser.java
+++ src/main/java/org/truffleruby/parser/parser/RubyParser.java
@@ -899,4 +899,187 @@ public class RubyParser {
 
   /** computes list of expected tokens on error by tracing the tables.
       @param state for which to compute the list.
-      @return list of
+      @return list of token names.
+    */
+  protected String[] yyExpecting (int state) {
+    int token, n, len = 0;
+    boolean[] ok = new boolean[yyNames.length];
+
+    if ((n = yySindex[state]) != 0)
+      for (token = n < 0 ? -n : 0;
+           token < yyNames.length && n+token < yyTable.length; ++ token)
+        if (yyCheck[n+token] == token && !ok[token] && yyNames[token] != null) {
+          ++ len;
+          ok[token] = true;
+        }
+    if ((n = yyRindex[state]) != 0)
+      for (token = n < 0 ? -n : 0;
+           token < yyNames.length && n+token < yyTable.length; ++ token)
+        if (yyCheck[n+token] == token && !ok[token] && yyNames[token] != null) {
+          ++ len;
+          ok[token] = true;
+        }
+
+    String result[] = new String[len];
+    for (n = token = 0; n < len;  ++ token)
+      if (ok[token]) result[n++] = yyNames[token];
+    return result;
+  }
+
+  /** the generated parser, with debugging messages.
+      Maintains a dynamic state and value stack.
+      @param yyLex scanner.
+      @return result of the last reduction, if any.
+    */
+  public Object yyparse (RubyLexer yyLex, Object ayydebug) {
+    return yyparse(yyLex);
+  }
+
+  /** initial size and increment of the state/value stack [default 256].
+      This is not final so that it can be overwritten outside of invocations
+      of {@link #yyparse}.
+    */
+  protected int yyMax;
+
+  /** executed at the beginning of a reduce action.
+      Used as <tt>$$ = yyDefault($1)</tt>, prior to the user-specified action, if any.
+      Can be overwritten to provide deep copy, etc.
+      @param first value for <tt>$1</tt>, or <tt>null</tt>.
+      @return first.
+    */
+  protected Object yyDefault (Object first) {
+    return first;
+  }
+
+  /** the generated parser.
+      Maintains a dynamic state and value stack.
+      @param yyLex scanner.
+      @return result of the last reduction, if any.
+    */
+  public Object yyparse (RubyLexer yyLex) {
+    if (yyMax <= 0) yyMax = 256;			// initial size
+    int yyState = 0, yyStates[] = new int[yyMax];	// state stack
+    Object yyVal = null, yyVals[] = new Object[yyMax];	// value stack
+    int yyToken = -1;					// current input
+    int yyErrorFlag = 0;				// #tokens to shift
+
+    yyLoop: for (int yyTop = 0;; ++ yyTop) {
+      if (yyTop >= yyStates.length) {			// dynamically increase
+        int[] i = new int[yyStates.length+yyMax];
+        System.arraycopy(yyStates, 0, i, 0, yyStates.length);
+        yyStates = i;
+        Object[] o = new Object[yyVals.length+yyMax];
+        System.arraycopy(yyVals, 0, o, 0, yyVals.length);
+        yyVals = o;
+      }
+      yyStates[yyTop] = yyState;
+      yyVals[yyTop] = yyVal;
+
+      yyDiscarded: for (;;) {	// discarding a token does not change stack
+        int yyN;
+        if ((yyN = yyDefRed[yyState]) == 0) {	// else [default] reduce (yyN)
+          if (yyToken < 0) {
+//            yyToken = yyLex.advance() ? yyLex.token() : 0;
+            yyToken = yyLex.nextToken();
+          }
+          if ((yyN = yySindex[yyState]) != 0 && (yyN += yyToken) >= 0
+              && yyN < yyTable.length && yyCheck[yyN] == yyToken) {
+            yyState = yyTable[yyN];		// shift to yyN
+            yyVal = yyLex.value();
+            yyToken = -1;
+            if (yyErrorFlag > 0) -- yyErrorFlag;
+            continue yyLoop;
+          }
+          if ((yyN = yyRindex[yyState]) != 0 && (yyN += yyToken) >= 0
+              && yyN < yyTable.length && yyCheck[yyN] == yyToken)
+            yyN = yyTable[yyN];			// reduce (yyN)
+          else
+            switch (yyErrorFlag) {
+  
+            case 0:
+              support.yyerror("syntax error", yyExpecting(yyState), yyNames[yyToken]);
+              break;
+  
+            case 1: case 2:
+              yyErrorFlag = 3;
+              do {
+                if ((yyN = yySindex[yyStates[yyTop]]) != 0
+                    && (yyN += yyErrorCode) >= 0 && yyN < yyTable.length
+                    && yyCheck[yyN] == yyErrorCode) {
+                  yyState = yyTable[yyN];
+                  yyVal = yyLex.value();
+                  continue yyLoop;
+                }
+              } while (-- yyTop >= 0);
+              support.yyerror("irrecoverable syntax error");
+              break;
+
+            case 3:
+              if (yyToken == 0) {
+                support.yyerror("irrecoverable syntax error at end-of-file");
+              }
+              yyToken = -1;
+              continue yyDiscarded;		// leave stack alone
+            }
+        }
+        int yyV = yyTop + 1-yyLen[yyN];
+        ParserState state = states[yyN];
+        if (state == null) {
+            yyVal = yyDefault(yyV > yyTop ? null : yyVals[yyV]);
+        } else {
+            yyVal = state.execute(support, lexer, yyVal, yyVals, yyTop);
+        }
+//        switch (yyN) {
+// ACTIONS_END
+//        }
+        yyTop -= yyLen[yyN];
+        yyState = yyStates[yyTop];
+        int yyM = yyLhs[yyN];
+        if (yyState == 0 && yyM == 0) {
+          yyState = yyFinal;
+          if (yyToken < 0) {
+            yyToken = yyLex.nextToken();
+//            yyToken = yyLex.advance() ? yyLex.token() : 0;
+          }
+          if (yyToken == 0) {
+            return yyVal;
+          }
+          continue yyLoop;
+        }
+        if ((yyN = yyGindex[yyM]) != 0 && (yyN += yyState) >= 0
+            && yyN < yyTable.length && yyCheck[yyN] == yyState)
+          yyState = yyTable[yyN];
+        else
+          yyState = yyDgoto[yyM];
+        continue yyLoop;
+      }
+    }
+  }
+
+static ParserState[] states = new ParserState[675];
+static {
+states[1] = (support, lexer, yyVal, yyVals, yyTop) -> {
+    lexer.setState(EXPR_BEG);
+    support.initTopLocalVariables();
+    return yyVal;
+};
+states[2] = (support, lexer, yyVal, yyVals, yyTop) -> {
+  /* ENEBO: Removed !compile_for_eval which probably is to reduce warnings*/
+                  if (((ParseNode)yyVals[0+yyTop]) != null) {
+                      /* last expression should not be void */
+                      if (((ParseNode)yyVals[0+yyTop]) instanceof BlockParseNode) {
+                          support.checkUselessStatement(((BlockParseNode)yyVals[0+yyTop]).getLast());
+                      } else {
+                          support.checkUselessStatement(((ParseNode)yyVals[0+yyTop]));
+                      }
+                  }
+                  support.getResult().setAST(support.addRootNode(((ParseNode)yyVals[0+yyTop])));
+    return yyVal;
+};
+states[3] = (support, lexer, yyVal, yyVals, yyTop) -> {
+    if (((ParseNode)yyVals[-1+yyTop]) instanceof BlockParseNode) {
+        support.checkUselessStatements(((BlockParseNode)yyVals[-1+yyTop]));
+    }
+    yyVal = ((ParseNode)yyVals[-1+yyTop]);
+    return yyVal;
+};
