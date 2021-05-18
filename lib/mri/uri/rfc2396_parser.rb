9967ed1a912e41da6e0aba67fd64794026be2bd4
seadu <68719109+seadu@users.noreply.github.com>
2021-05-18 21:59:45 +0000

    Read docs/CHANGELOG.txt

--- lib/mri/uri/rfc2396_parser.rb
+++ lib/mri/uri/rfc2396_parser.rb
@@ -335,4 +335,166 @@ module URI
     private
 
     # Constructs the default Hash of patterns.
- 
+    def initialize_pattern(opts = {})
+      ret = {}
+      ret[:ESCAPED] = escaped = (opts.delete(:ESCAPED) || PATTERN::ESCAPED)
+      ret[:UNRESERVED] = unreserved = opts.delete(:UNRESERVED) || PATTERN::UNRESERVED
+      ret[:RESERVED] = reserved = opts.delete(:RESERVED) || PATTERN::RESERVED
+      ret[:DOMLABEL] = opts.delete(:DOMLABEL) || PATTERN::DOMLABEL
+      ret[:TOPLABEL] = opts.delete(:TOPLABEL) || PATTERN::TOPLABEL
+      ret[:HOSTNAME] = hostname = opts.delete(:HOSTNAME)
+
+      # RFC 2396 (URI Generic Syntax)
+      # RFC 2732 (IPv6 Literal Addresses in URL's)
+      # RFC 2373 (IPv6 Addressing Architecture)
+
+      # uric          = reserved | unreserved | escaped
+      ret[:URIC] = uric = "(?:[#{unreserved}#{reserved}]|#{escaped})"
+      # uric_no_slash = unreserved | escaped | ";" | "?" | ":" | "@" |
+      #                 "&" | "=" | "+" | "$" | ","
+      ret[:URIC_NO_SLASH] = uric_no_slash = "(?:[#{unreserved};?:@&=+$,]|#{escaped})"
+      # query         = *uric
+      ret[:QUERY] = query = "#{uric}*"
+      # fragment      = *uric
+      ret[:FRAGMENT] = fragment = "#{uric}*"
+
+      # hostname      = *( domainlabel "." ) toplabel [ "." ]
+      # reg-name      = *( unreserved / pct-encoded / sub-delims ) # RFC3986
+      unless hostname
+        ret[:HOSTNAME] = hostname = "(?:[a-zA-Z0-9\\-.]|%\\h\\h)+"
+      end
+
+      # RFC 2373, APPENDIX B:
+      # IPv6address = hexpart [ ":" IPv4address ]
+      # IPv4address   = 1*3DIGIT "." 1*3DIGIT "." 1*3DIGIT "." 1*3DIGIT
+      # hexpart = hexseq | hexseq "::" [ hexseq ] | "::" [ hexseq ]
+      # hexseq  = hex4 *( ":" hex4)
+      # hex4    = 1*4HEXDIG
+      #
+      # XXX: This definition has a flaw. "::" + IPv4address must be
+      # allowed too.  Here is a replacement.
+      #
+      # IPv4address = 1*3DIGIT "." 1*3DIGIT "." 1*3DIGIT "." 1*3DIGIT
+      ret[:IPV4ADDR] = ipv4addr = "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"
+      # hex4     = 1*4HEXDIG
+      hex4 = "[#{PATTERN::HEX}]{1,4}"
+      # lastpart = hex4 | IPv4address
+      lastpart = "(?:#{hex4}|#{ipv4addr})"
+      # hexseq1  = *( hex4 ":" ) hex4
+      hexseq1 = "(?:#{hex4}:)*#{hex4}"
+      # hexseq2  = *( hex4 ":" ) lastpart
+      hexseq2 = "(?:#{hex4}:)*#{lastpart}"
+      # IPv6address = hexseq2 | [ hexseq1 ] "::" [ hexseq2 ]
+      ret[:IPV6ADDR] = ipv6addr = "(?:#{hexseq2}|(?:#{hexseq1})?::(?:#{hexseq2})?)"
+
+      # IPv6prefix  = ( hexseq1 | [ hexseq1 ] "::" [ hexseq1 ] ) "/" 1*2DIGIT
+      # unused
+
+      # ipv6reference = "[" IPv6address "]" (RFC 2732)
+      ret[:IPV6REF] = ipv6ref = "\\[#{ipv6addr}\\]"
+
+      # host          = hostname | IPv4address
+      # host          = hostname | IPv4address | IPv6reference (RFC 2732)
+      ret[:HOST] = host = "(?:#{hostname}|#{ipv4addr}|#{ipv6ref})"
+      # port          = *digit
+      ret[:PORT] = port = '\d*'
+      # hostport      = host [ ":" port ]
+      ret[:HOSTPORT] = hostport = "#{host}(?::#{port})?"
+
+      # userinfo      = *( unreserved | escaped |
+      #                    ";" | ":" | "&" | "=" | "+" | "$" | "," )
+      ret[:USERINFO] = userinfo = "(?:[#{unreserved};:&=+$,]|#{escaped})*"
+
+      # pchar         = unreserved | escaped |
+      #                 ":" | "@" | "&" | "=" | "+" | "$" | ","
+      pchar = "(?:[#{unreserved}:@&=+$,]|#{escaped})"
+      # param         = *pchar
+      param = "#{pchar}*"
+      # segment       = *pchar *( ";" param )
+      segment = "#{pchar}*(?:;#{param})*"
+      # path_segments = segment *( "/" segment )
+      ret[:PATH_SEGMENTS] = path_segments = "#{segment}(?:/#{segment})*"
+
+      # server        = [ [ userinfo "@" ] hostport ]
+      server = "(?:#{userinfo}@)?#{hostport}"
+      # reg_name      = 1*( unreserved | escaped | "$" | "," |
+      #                     ";" | ":" | "@" | "&" | "=" | "+" )
+      ret[:REG_NAME] = reg_name = "(?:[#{unreserved}$,;:@&=+]|#{escaped})+"
+      # authority     = server | reg_name
+      authority = "(?:#{server}|#{reg_name})"
+
+      # rel_segment   = 1*( unreserved | escaped |
+      #                     ";" | "@" | "&" | "=" | "+" | "$" | "," )
+      ret[:REL_SEGMENT] = rel_segment = "(?:[#{unreserved};@&=+$,]|#{escaped})+"
+
+      # scheme        = alpha *( alpha | digit | "+" | "-" | "." )
+      ret[:SCHEME] = scheme = "[#{PATTERN::ALPHA}][\\-+.#{PATTERN::ALPHA}\\d]*"
+
+      # abs_path      = "/"  path_segments
+      ret[:ABS_PATH] = abs_path = "/#{path_segments}"
+      # rel_path      = rel_segment [ abs_path ]
+      ret[:REL_PATH] = rel_path = "#{rel_segment}(?:#{abs_path})?"
+      # net_path      = "//" authority [ abs_path ]
+      ret[:NET_PATH] = net_path = "//#{authority}(?:#{abs_path})?"
+
+      # hier_part     = ( net_path | abs_path ) [ "?" query ]
+      ret[:HIER_PART] = hier_part = "(?:#{net_path}|#{abs_path})(?:\\?(?:#{query}))?"
+      # opaque_part   = uric_no_slash *uric
+      ret[:OPAQUE_PART] = opaque_part = "#{uric_no_slash}#{uric}*"
+
+      # absoluteURI   = scheme ":" ( hier_part | opaque_part )
+      ret[:ABS_URI] = abs_uri = "#{scheme}:(?:#{hier_part}|#{opaque_part})"
+      # relativeURI   = ( net_path | abs_path | rel_path ) [ "?" query ]
+      ret[:REL_URI] = rel_uri = "(?:#{net_path}|#{abs_path}|#{rel_path})(?:\\?#{query})?"
+
+      # URI-reference = [ absoluteURI | relativeURI ] [ "#" fragment ]
+      ret[:URI_REF] = "(?:#{abs_uri}|#{rel_uri})?(?:##{fragment})?"
+
+      ret[:X_ABS_URI] = "
+        (#{scheme}):                           (?# 1: scheme)
+        (?:
+           (#{opaque_part})                    (?# 2: opaque)
+        |
+           (?:(?:
+             //(?:
+                 (?:(?:(#{userinfo})@)?        (?# 3: userinfo)
+                   (?:(#{host})(?::(\\d*))?))? (?# 4: host, 5: port)
+               |
+                 (#{reg_name})                 (?# 6: registry)
+               )
+             |
+             (?!//))                           (?# XXX: '//' is the mark for hostport)
+             (#{abs_path})?                    (?# 7: path)
+           )(?:\\?(#{query}))?                 (?# 8: query)
+        )
+        (?:\\#(#{fragment}))?                  (?# 9: fragment)
+      "
+
+      ret[:X_REL_URI] = "
+        (?:
+          (?:
+            //
+            (?:
+              (?:(#{userinfo})@)?       (?# 1: userinfo)
+                (#{host})?(?::(\\d*))?  (?# 2: host, 3: port)
+            |
+              (#{reg_name})             (?# 4: registry)
+            )
+          )
+        |
+          (#{rel_segment})              (?# 5: rel_segment)
+        )?
+        (#{abs_path})?                  (?# 6: abs_path)
+        (?:\\?(#{query}))?              (?# 7: query)
+        (?:\\#(#{fragment}))?           (?# 8: fragment)
+      "
+
+      ret
+    end
+
+    # Constructs the default Hash of Regexp's.
+    def initialize_regexp(pattern)
+      ret = {}
+
+      # for URI::split
+      ret[:ABS_URI] = Regexp.new('\A\s*' + pattern[:X_
