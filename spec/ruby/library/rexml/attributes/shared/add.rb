63b299c75306c130f38a0352d19fc351b948b667
seadu <68719109+seadu@users.noreply.github.com>
2021-06-11 19:56:42 +0000

    Fix CS error

--- spec/ruby/library/rexml/attributes/shared/add.rb
+++ spec/ruby/library/rexml/attributes/shared/add.rb
@@ -7,4 +7,11 @@ describe :rexml_attribute_add, shared: true do
 
   it "adds an attribute" do
     @attr.send(@method, @name)
-    @attr["na
+    @attr["name"].should == "Joe"
+  end
+
+  it "replaces an existing attribute" do
+    @attr.send(@method, REXML::Attribute.new("name", "Bruce"))
+    @attr["name"].should == "Bruce"
+  end
+end
---
+++ src/main/java/org/truffleruby/language/control/UnlessNode.java
@@ -0,0 +1,73 @@
+
+/*
+ * Copyright (c) 2013, 2023 Oracle and/or its affiliates. All rights reserved. This
+ * code is released under a tri EPL/GPL/LGPL license. You can use it,
+ * redistribute it and/or modify it under the terms of the:
+ *
+ * Eclipse Public License version 2.0, or
+ * GNU General Public License version 2, or
+ * GNU Lesser General Public License version 2.1.
+ */
+package org.truffleruby.language.control;
+
+import com.oracle.truffle.api.profiles.CountingConditionProfile;
+import org.truffleruby.core.cast.BooleanCastNode;
+import org.truffleruby.core.cast.BooleanCastNodeGen;
+import org.truffleruby.language.RubyContextSourceNode;
+import org.truffleruby.language.RubyNode;
+
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+public class UnlessNode extends RubyContextSourceNode {
+
+    @Child private BooleanCastNode condition;
+    @Child private RubyNode thenBody;
+
+    private final CountingConditionProfile conditionProfile = CountingConditionProfile.create();
+
+    public UnlessNode(RubyNode condition, RubyNode thenBody) {
+        this.condition = BooleanCastNodeGen.create(condition);
+        this.thenBody = thenBody;
+    }
+
+    public UnlessNode(BooleanCastNode condition, RubyNode thenBody) {
+        this.condition = condition;
+        this.thenBody = thenBody;
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        if (!conditionProfile.profile(condition.execute(frame))) {
+            return thenBody.execute(frame);
+        } else {
+            return nil;
+        }
+    }
+
+    @Override
+    public boolean canSubsumeFollowing() {
+        return !thenBody.isContinuable();
+    }
+
+    @Override
+    public RubyNode subsumeFollowing(RubyNode following) {
+        return new IfElseNode(condition, following, thenBody).copySourceSection(this);
+    }
+
+    @Override
+    public RubyNode simplifyAsTailExpression() {
+        return new UnlessNode(condition, thenBody.simplifyAsTailExpression()).copySourceSection(this);
+    }
+
+    private RubyNode getConditionBeforeCasting() {
+        return condition.getValueNode();
+    }
+
+    public RubyNode cloneUninitialized() {
+        var copy = new UnlessNode(
+                getConditionBeforeCasting().cloneUninitialized(),
+                thenBody.cloneUninitialized());
+        return copy.copyFlags(this);
+    }
+
+}
