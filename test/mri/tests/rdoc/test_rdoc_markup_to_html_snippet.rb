34badde16faac3b61a79aac4b6e337a2a6e72270
seadu <68719109+seadu@users.noreply.github.com>
2022-05-02 16:58:22 +0000

    v0.1.12

--- test/mri/tests/rdoc/test_rdoc_markup_to_html_snippet.rb
+++ test/mri/tests/rdoc/test_rdoc_markup_to_html_snippet.rb
@@ -245,4 +245,308 @@ class TestRDocMarkupToHtmlSnippet < RDoc::Markup::FormatterTestCase
 
   def accept_list_start_ualpha
     assert_equal [:UALPHA], @to.list
-    assert_eq
+    assert_equal [''], @to.in_list_entry
+
+    assert_equal '', @to.res.join
+    assert_equal 0, @to.characters
+  end
+
+  def accept_paragraph
+    assert_equal "<p>hi\n", @to.res.join
+
+    assert_equal 2, @to.characters
+  end
+
+  def accept_paragraph_b
+    assert_equal "<p>reg <strong>bold words</strong> reg\n", @to.res.join
+
+    assert_equal 18, @to.characters
+  end
+
+  def accept_paragraph_br
+    assert_equal "<p>one<br>two\n", @to.res.join
+
+    assert_equal 6, @to.characters
+  end
+
+  def accept_paragraph_break
+    assert_equal "<p>hello<br>\nworld\n", @to.res.join
+
+    assert_equal 11, @to.characters
+  end
+
+  def accept_paragraph_i
+    assert_equal "<p>reg <em>italic words</em> reg\n", @to.res.join
+
+    assert_equal 20, @to.characters
+  end
+
+  def accept_paragraph_plus
+    assert_equal "<p>reg <code>teletype</code> reg\n", @to.res.join
+
+    assert_equal 16, @to.characters
+  end
+
+  def accept_paragraph_star
+    assert_equal "<p>reg <strong>bold</strong> reg\n", @to.res.join
+
+    assert_equal 12, @to.characters
+  end
+
+  def accept_paragraph_underscore
+    assert_equal "<p>reg <em>italic</em> reg\n", @to.res.join
+
+    assert_equal 14, @to.characters
+  end
+
+  def accept_raw
+    assert_equal '', @to.res.join
+    assert_equal 0, @to.characters
+  end
+
+  def accept_rule
+    assert_empty @to.res
+    assert_equal 0, @to.characters
+  end
+
+  def accept_verbatim
+    assert_equal "\n<pre class=\"ruby\"><span class=\"ruby-identifier\">hi</span>\n  <span class=\"ruby-identifier\">world</span>\n</pre>\n", @to.res.join
+    assert_equal 10, @to.characters
+  end
+
+  def end_accepting
+    assert_equal 'hi', @to.res.join
+  end
+
+  def start_accepting
+    assert_equal [], @to.res
+    assert_equal [], @to.in_list_entry
+    assert_equal [], @to.list
+    assert_equal 0,  @to.characters
+  end
+
+  def list_nested
+    expected = <<-EXPECTED
+<p>l1
+<p>l1.1
+
+<p>l2
+
+    EXPECTED
+
+    assert_equal expected, @to.res.join
+    assert_equal 8, @to.characters
+  end
+
+  def list_verbatim
+    expected = <<-EXPECTED
+<p>list stuff
+
+<pre>* list
+  with
+
+  second
+
+  1. indented
+  2. numbered
+
+  third
+
+* second</pre>
+
+    EXPECTED
+
+    assert_equal expected, @to.end_accepting
+    assert_equal 81, @to.characters
+  end
+
+  def test_accept_heading_7
+    @to.start_accepting
+
+    @to.accept_heading @RM::Heading.new(7, 'Hello')
+
+    assert_equal "<p>Hello\n", @to.res.join
+    assert_equal 5, @to.characters
+  end
+
+  def test_accept_heading_aref_class
+    @to.code_object = RDoc::NormalClass.new 'Foo'
+    @to.start_accepting
+
+    @to.accept_heading @RM::Heading.new(1, 'Hello')
+
+    assert_equal "<p>Hello\n",
+                 @to.res.join
+    assert_equal 5, @to.characters
+  end
+
+  def test_accept_heading_aref_method
+    @to.code_object = RDoc::AnyMethod.new nil, 'foo'
+    @to.start_accepting
+
+    @to.accept_heading @RM::Heading.new(1, 'Hello')
+
+    assert_equal "<p>Hello\n",
+                 @to.res.join
+    assert_equal 5, @to.characters
+  end
+
+  def test_accept_verbatim_ruby
+    options = RDoc::Options.new
+    rdoc = RDoc::RDoc.new
+    rdoc.options = options
+    RDoc::RDoc.current = rdoc
+
+    verb = @RM::Verbatim.new("class C\n", "end\n")
+
+    @to.start_accepting
+    @to.accept_verbatim verb
+
+    expected = <<-EXPECTED
+
+<pre class="ruby"><span class="ruby-keyword">class</span> <span class="ruby-constant">C</span>
+<span class="ruby-keyword">end</span>
+</pre>
+    EXPECTED
+
+    assert_equal expected, @to.res.join
+    assert_equal 11, @to.characters
+  end
+
+  def test_accept_verbatim_ruby_error
+    options = RDoc::Options.new
+    rdoc = RDoc::RDoc.new
+    rdoc.options = options
+    RDoc::RDoc.current = rdoc
+
+    verb = @RM::Verbatim.new("a % 09 # => blah\n")
+
+    @to.start_accepting
+    @to.accept_verbatim verb
+
+    inner = CGI.escapeHTML "a % 09 # => blah"
+
+    expected = <<-EXPECTED
+
+<pre>#{inner}</pre>
+    EXPECTED
+
+    assert_equal expected, @to.res.join
+    assert_equal 16, @to.characters
+  end
+
+  def test_add_paragraph
+    @to = RDoc::Markup::ToHtmlSnippet.new @options, 0, 3
+    assert_throws :done do
+      @to.add_paragraph
+      @to.add_paragraph
+      @to.add_paragraph
+    end
+
+    assert_equal 3, @to.paragraphs
+  end
+
+  def test_convert_limit
+    rdoc = <<-RDOC
+= Hello
+
+This is some text, it *will* be cut off after 100 characters and an ellipsis
+must follow
+
+So there you have it
+    RDOC
+
+    expected = <<-EXPECTED
+<p>Hello
+<p>This is some text, it <strong>will</strong> be cut off after 100 characters and an ellipsis must follow
+<p>So there you #{@ellipsis}
+    EXPECTED
+
+    actual = @to.convert rdoc
+
+    assert_equal expected, actual
+    assert_equal 111, @to.characters, 'snippet character length'
+  end
+
+  def test_convert_limit_2
+    rdoc = <<-RDOC
+Outputs formatted RI data for the class or method +name+.
+
+Returns true if +name+ was found, false if it was not an alternative could
+be guessed, raises an error if +name+ couldn't be guessed.
+    RDOC
+
+    expected = <<-EXPECTED
+<p>Outputs formatted RI data for the class or method <code>name</code>.
+<p>Returns true if <code>name</code> was found, false if it was #{@ellipsis}
+    EXPECTED
+
+    actual = @to.convert rdoc
+
+    assert_equal expected, actual
+    assert_equal 159, @to.characters, 'snippet character length'
+  end
+
+  def test_convert_limit_paragraphs
+    @to = RDoc::Markup::ToHtmlSnippet.new @options, 100, 3
+
+    rdoc = <<-RDOC
+= \RDoc - Ruby Documentation System
+
+* {RDoc Project Page}[https://github.com/ruby/rdoc]
+* {RDoc Documentation}[https://ruby.github.io/rdoc]
+* {RDoc Bug Tracker}[https://github.com/ruby/rdoc/issues]
+
+== DESCRIPTION:
+
+RDoc produces HTML and command-line documentation for Ruby projects.  RDoc
+includes the +rdoc+ and +ri+ tools for generating and displaying online
+documentation.
+
+See RDoc for a description of RDoc's markup and basic use.
+    RDOC
+
+    expected = <<-EXPECTED
+<p>RDoc - Ruby Documentation System
+<p>RDoc Project Page
+<p>RDoc Documentation
+    EXPECTED
+
+    actual = @to.convert rdoc
+
+    assert_equal expected, actual
+    assert_equal 67, @to.characters
+  end
+
+  def test_convert_limit_in_tag
+    @to = RDoc::Markup::ToHtmlSnippet.new @options, 4
+    rdoc = "* ab *c* d\n"
+
+    expected = "<p>ab <strong>c</strong> #{@ellipsis}\n\n"
+
+    actual = @to.convert rdoc
+
+    assert_equal 4, @to.characters
+    assert_equal expected, actual
+  end
+
+  def test_convert_limit_verbatim
+    rdoc = <<-RDOC
+= Hello There
+
+This is some text, it *will* be cut off after 100 characters
+
+  This one is cut off in this verbatim section
+    RDOC
+
+    expected = <<-EXPECTED
+<p>Hello There
+<p>This is some text, it <strong>will</strong> be cut off after 100 characters
+
+<pre>This one is cut off in this verbatim ...</pre>
+    EXPECTED
+
+    actual = @to.convert rdoc
+
+    assert_equal expected, actual
+    assert_equal
