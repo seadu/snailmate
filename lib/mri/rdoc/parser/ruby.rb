9586c2eecebc5f57a80017b7835452c8ab4b1dfa
seadu <68719109+seadu@users.noreply.github.com>
2022-08-21 22:28:42 +0000

    Delete anti_spam.lua

--- lib/mri/rdoc/parser/ruby.rb
+++ lib/mri/rdoc/parser/ruby.rb
@@ -347,4 +347,264 @@ class RDoc::Parser::Ruby < RDoc::Parser
     name_t = get_tk
     given_name = ''.dup
 
-    # class ::A -> A is in the top
+    # class ::A -> A is in the top level
+    if :on_op == name_t[:kind] and '::' == name_t[:text] then # bug
+      name_t = get_tk
+      container = @top_level
+      given_name << '::'
+    end
+
+    skip_tkspace_without_nl
+    given_name << name_t[:text]
+
+    is_self = name_t[:kind] == :on_op && name_t[:text] == '<<'
+    new_modules = []
+    while !is_self && (tk = peek_tk) and :on_op == tk[:kind] and '::' == tk[:text] do
+      prev_container = container
+      container = container.find_module_named name_t[:text]
+      container ||=
+        if ignore_constants then
+          c = RDoc::NormalModule.new name_t[:text]
+          c.store = @store
+          new_modules << [prev_container, c]
+          c
+        else
+          c = prev_container.add_module RDoc::NormalModule, name_t[:text]
+          c.ignore unless prev_container.document_children
+          @top_level.add_to_classes_or_modules c
+          c
+        end
+
+      record_location container
+
+      get_tk
+      skip_tkspace
+      if :on_lparen == peek_tk[:kind] # ProcObjectInConstant::()
+        parse_method_or_yield_parameters
+        break
+      end
+      name_t = get_tk
+      unless :on_const == name_t[:kind] || :on_ident == name_t[:kind]
+        raise RDoc::Error, "Invalid class or module definition: #{given_name}"
+      end
+      if prev_container == container and !ignore_constants
+        given_name = name_t[:text]
+      else
+        given_name << '::' + name_t[:text]
+      end
+    end
+
+    skip_tkspace_without_nl
+
+    return [container, name_t, given_name, new_modules]
+  end
+
+  ##
+  # Return a superclass, which can be either a constant of an expression
+
+  def get_class_specification
+    tk = peek_tk
+    if tk.nil?
+      return ''
+    elsif :on_kw == tk[:kind] && 'self' == tk[:text]
+      return 'self'
+    elsif :on_gvar == tk[:kind]
+      return ''
+    end
+
+    res = get_constant
+
+    skip_tkspace_without_nl
+
+    get_tkread # empty out read buffer
+
+    tk = get_tk
+    return res unless tk
+
+    case tk[:kind]
+    when :on_nl, :on_comment, :on_embdoc, :on_semicolon then
+      unget_tk(tk)
+      return res
+    end
+
+    res += parse_call_parameters(tk)
+    res
+  end
+
+  ##
+  # Parse a constant, which might be qualified by one or more class or module
+  # names
+
+  def get_constant
+    res = ""
+    skip_tkspace_without_nl
+    tk = get_tk
+
+    while tk && ((:on_op == tk[:kind] && '::' == tk[:text]) || :on_const == tk[:kind]) do
+      res += tk[:text]
+      tk = get_tk
+    end
+
+    unget_tk(tk)
+    res
+  end
+
+  ##
+  # Get an included module that may be surrounded by parens
+
+  def get_included_module_with_optional_parens
+    skip_tkspace_without_nl
+    get_tkread
+    tk = get_tk
+    end_token = get_end_token tk
+    return '' unless end_token
+
+    nest = 0
+    continue = false
+    only_constant = true
+
+    while tk != nil do
+      is_element_of_constant = false
+      case tk[:kind]
+      when :on_semicolon then
+        break if nest == 0
+      when :on_lbracket then
+        nest += 1
+      when :on_rbracket then
+        nest -= 1
+      when :on_lbrace then
+        nest += 1
+      when :on_rbrace then
+        nest -= 1
+        if nest <= 0
+          # we might have a.each { |i| yield i }
+          unget_tk(tk) if nest < 0
+          break
+        end
+      when :on_lparen then
+        nest += 1
+      when end_token[:kind] then
+        if end_token[:kind] == :on_rparen
+          nest -= 1
+          break if nest <= 0
+        else
+          break if nest <= 0
+        end
+      when :on_rparen then
+        nest -= 1
+      when :on_comment, :on_embdoc then
+        @read.pop
+        if :on_nl == end_token[:kind] and "\n" == tk[:text][-1] and
+          (!continue or (tk[:state] & RDoc::Parser::RipperStateLex::EXPR_LABEL) != 0) then
+          break if !continue and nest <= 0
+        end
+      when :on_comma then
+        continue = true
+      when :on_ident then
+        continue = false if continue
+      when :on_kw then
+        case tk[:text]
+        when 'def', 'do', 'case', 'for', 'begin', 'class', 'module'
+          nest += 1
+        when 'if', 'unless', 'while', 'until', 'rescue'
+          # postfix if/unless/while/until/rescue must be EXPR_LABEL
+          nest += 1 unless (tk[:state] & RDoc::Parser::RipperStateLex::EXPR_LABEL) != 0
+        when 'end'
+          nest -= 1
+          break if nest == 0
+        end
+      when :on_const then
+        is_element_of_constant = true
+      when :on_op then
+        is_element_of_constant = true if '::' == tk[:text]
+      end
+      only_constant = false unless is_element_of_constant
+      tk = get_tk
+    end
+
+    if only_constant
+      get_tkread_clean(/\s+/, ' ')
+    else
+      ''
+    end
+  end
+
+  ##
+  # Little hack going on here. In the statement:
+  #
+  #   f = 2*(1+yield)
+  #
+  # We see the RPAREN as the next token, so we need to exit early.  This still
+  # won't catch all cases (such as "a = yield + 1"
+
+  def get_end_token tk # :nodoc:
+    case tk[:kind]
+    when :on_lparen
+      token = RDoc::Parser::RipperStateLex::Token.new
+      token[:kind] = :on_rparen
+      token[:text] = ')'
+      token
+    when :on_rparen
+      nil
+    else
+      token = RDoc::Parser::RipperStateLex::Token.new
+      token[:kind] = :on_nl
+      token[:text] = "\n"
+      token
+    end
+  end
+
+  ##
+  # Retrieves the method container for a singleton method.
+
+  def get_method_container container, name_t # :nodoc:
+    prev_container = container
+    container = container.find_module_named(name_t[:text])
+
+    unless container then
+      constant = prev_container.constants.find do |const|
+        const.name == name_t[:text]
+      end
+
+      if constant then
+        parse_method_dummy prev_container
+        return
+      end
+    end
+
+    unless container then
+      # TODO seems broken, should starting at Object in @store
+      obj = name_t[:text].split("::").inject(Object) do |state, item|
+        state.const_get(item)
+      end rescue nil
+
+      type = obj.class == Class ? RDoc::NormalClass : RDoc::NormalModule
+
+      unless [Class, Module].include?(obj.class) then
+        warn("Couldn't find #{name_t[:text]}. Assuming it's a module")
+      end
+
+      if type == RDoc::NormalClass then
+        sclass = obj.superclass ? obj.superclass.name : nil
+        container = prev_container.add_class type, name_t[:text], sclass
+      else
+        container = prev_container.add_module type, name_t[:text]
+      end
+
+      record_location container
+    end
+
+    container
+  end
+
+  ##
+  # Extracts a name or symbol from the token stream.
+
+  def get_symbol_or_name
+    tk = get_tk
+    case tk[:kind]
+    when :on_symbol then
+      text = tk[:text].sub(/^:/, '')
+
+      next_tk = peek_tk
+      if next_tk && :on_op == next_tk[:kind] && '=' 
