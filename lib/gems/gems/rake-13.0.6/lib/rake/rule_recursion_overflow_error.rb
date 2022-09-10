687515e732721aca2308221e881608d9e9c91dd4
seadu <68719109+seadu@users.noreply.github.com>
2022-09-10 22:18:14 +0000

    version 0.0.11

--- lib/gems/gems/rake-13.0.6/lib/rake/rule_recursion_overflow_error.rb
+++ lib/gems/gems/rake-13.0.6/lib/rake/rule_recursion_overflow_error.rb
@@ -2,4 +2,19 @@
 module Rake
 
   # Error indicating a recursion overflow error in task selection.
-  class RuleRecursionOverflow
+  class RuleRecursionOverflowError < StandardError
+    def initialize(*args)
+      super
+      @targets = []
+    end
+
+    def add_target(target)
+      @targets << target
+    end
+
+    def message
+      super + ": [" + @targets.reverse.join(" => ") + "]"
+    end
+  end
+
+end
